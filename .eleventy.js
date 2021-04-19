0d0a83611306687b16331962cda36e5d7bbc8314
dmitrydao <dmitrydaopro@gmail.com>
2021-04-19 10:42:30 +0400

    install and configure youtube-embed plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function (eleventyConfig) {
   const rdmd = require('@readme/markdown')
+  const embedYouTube = require("eleventy-plugin-youtube-embed");
   eleventyConfig.setLibrary('md', { render: rdmd.html })
 
   eleventyConfig.addFilter('squash', require('./_includes/squash.js'))
+  eleventyConfig.addPlugin(embedYouTube)
   eleventyConfig.addPassthroughCopy({ _src: '/' })
 }
