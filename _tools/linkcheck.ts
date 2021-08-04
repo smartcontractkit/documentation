a1234bed888897e63a23557aee3eae1c328d745a
Stephen Fluin <stephen.fluin@gmail.com>
2021-08-04 16:27:15 -0500

    fix keepers links

--- _tools/linkcheck.ts
+++ _tools/linkcheck.ts
@@ -1,13 +1,18 @@
-import { platform, exit } from 'process';
+import { platform, exit, argv, stdout } from 'process';
 import { spawn } from 'child_process';
 
 const server = spawn('yarn', ['serve'], {
   stdio: ['ignore', 'pipe', 'ignore'],
 });
 
+let external = false;
+if(argv[argv.length - 1] === '-e') {
+  external = true;
+}
+
 let output = '';
 server.stdout.on('data', (data) => {
-  console.log(data.toString());
+  stdout.write(data.toString());
   output = output + data.toString();
   if (output.indexOf('Serving files from') !== -1) {
     let program;
@@ -18,11 +23,16 @@ server.stdout.on('data', (data) => {
     } else {
       program = 'linkcheck-win';
     }
-    const checker = spawn(program, [':4200'], {
+    const args = [':4200'];
+    if(external) {
+        args.push('-e');
+        console.log('Also checking external links.');
+    }
+    const checker = spawn(program, args, {
       stdio: ['ignore', 'pipe', 'ignore'],
     });
     checker.stdout.on('data', (checkerData) => {
-      console.log(checkerData.toString());
+      stdout.write(checkerData.toString());
     });
     checker.on('exit', (code, other) => {
       console.log('linkcheck finished');
