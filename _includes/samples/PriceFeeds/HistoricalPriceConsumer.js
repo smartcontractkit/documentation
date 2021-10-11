69bf318590adaedaad33d043e226f853284230cf
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2021-10-11 08:05:18 -0400

    hide infura key and add ethers examples

--- _includes/samples/PriceFeeds/HistoricalPriceConsumer.js
+++ _includes/samples/PriceFeeds/HistoricalPriceConsumer.js
@@ -1,11 +1,12 @@
-const web3 = new Web3("https://kovan.infura.io/v3/34ed41c4cf28406885f032930d670036");
-const aggregatorV3InterfaceABI = [{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
-const addr = "0x9326BFA02ADD2366b30bacB125260Af641031331";
-const priceFeed = new web3.eth.Contract(aggregatorV3InterfaceABI, addr);
+const Web3 = require("web3") // for nodejs only
+const web3 = new Web3("https://kovan.infura.io/v3/<infura_project_id>")
+const aggregatorV3InterfaceABI = [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "description", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint80", "name": "_roundId", "type": "uint80" }], "name": "getRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "latestRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
+const addr = "0x9326BFA02ADD2366b30bacB125260Af641031331"
+const priceFeed = new web3.eth.Contract(aggregatorV3InterfaceABI, addr)
 
 // Valid roundId must be known. They are NOT incremental.
-let validId = BigInt("18446744073709562301");
+let validId = BigInt("18446744073709562301")
 priceFeed.methods.getRoundData(validId).call()
-.then((historicalRoundData) => {
-document.getElementById('get-price-field').value = historicalRoundData.answer;
- })
+    .then((historicalRoundData) => {
+        document.getElementById('get-price-field').value = historicalRoundData.answer
+    })
