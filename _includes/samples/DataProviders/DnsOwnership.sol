e25ffff6add02a8b6c55844861ee2825e90827d4
1marcghannam <1marc.ghannam@gmail.com>
2022-04-06 04:00:29 -0400

    Update dns-ownership documentation. (#652)
    * Update dns-ownership documentation.
    
    1. Adapter has been updated: Passing type as TXT is no longer needed.
    2. Ethereum job cost changed from 1 LINK to 2 LINK.
    
    * Update Kovan Oracle Address
    
    Update Kovan Oracle address
    
    * Update DnsOwnership.sol
    
    Update DNSOwnership example
    
    * Update dns-ownership-oracle.md
    
    Update the links and the main contract. Transform the page to one click tutorial easy to use
    
    * Update dns-ownership-oracle.md
    
    * Update dns-ownership-oracle.md
    
    * Update dns-ownership-oracle.md
    
    Document the V2 Job
    
    * Minor edits
    
    Co-authored-by: Amine E <aelmanaa@users.noreply.github.com>
    Co-authored-by: Dwight Lyle <dwightjl@gmail.com>

--- _includes/samples/DataProviders/DnsOwnership.sol
+++ _includes/samples/DataProviders/DnsOwnership.sol
@@ -13,42 +13,49 @@ contract DnsOwnershipChainlink is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
     
     uint256 oraclePayment;
+    address private oracle;
+    bytes32 private jobId;
     bool public proof;
     /**
      * Network: Kovan
      * Oracle: 
      *      Name:           LinkPool
      *      Listing URL:    https://market.link/nodes/323602b9-3831-4f8d-a66b-3fb7531649eb?network=42
-     *      Address:        0x56dd6586DB0D08c6Ce7B2f2805af28616E082455
+     *      Address:        0xfF07C97631Ff3bAb5e5e5660Cdf47AdEd8D4d4Fd
      * Job: 
      *      Name:           DNS Record Check
      *      Listing URL:    https://market.link/jobs/bf1a410f-ce93-497d-83ac-e63fed9d83bd?network=42
      *      ID:             791bd73c8a1349859f09b1cb87304f71
      *      Fee:            0.1 LINK
      */
-    constructor(uint256 _oraclePayment) ConfirmedOwner(msg.sender) {
+    constructor() ConfirmedOwner(msg.sender) {
       setPublicChainlinkToken();
-      oraclePayment = _oraclePayment;
+      oracle = 0xfF07C97631Ff3bAb5e5e5660Cdf47AdEd8D4d4Fd;
+      jobId = "791bd73c8a1349859f09b1cb87304f71";
+      oraclePayment = 0.1 * 10 ** 18; // (Varies by network and job)
     }
-
+    
+    /**
+    *  Request proof to check if the address `_record` owns the DNS name `_name`
+    *
+    */
     function requestProof
     (
-      address _oracle,
-      bytes32 _jobId,
-      string memory _txt,
       string memory _name,
       string memory _record
     ) 
       public 
       onlyOwner 
     {
-      Chainlink.Request memory req = buildChainlinkRequest(_jobId, address(this), this.fulfill.selector);
-      req.add("type", _txt);
+      Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
       req.add("name", _name);
       req.add("record", _record);
-      sendChainlinkRequestTo(_oracle, req, oraclePayment);
+      sendChainlinkRequestTo(oracle, req, oraclePayment);
     }
-
+    /**
+    * Callback function called by the oracle to submit the `_proof` . If true, this means that the address owns the dns name
+    *
+    */
     function fulfill(bytes32 _requestId, bool _proof)
       public
       recordChainlinkFulfillment(_requestId)
