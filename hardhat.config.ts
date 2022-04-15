1049d44abe507f84042164a3e728c385935e5443
aelmanaa <aelmanaa@aelmanaa>
2022-04-15 16:15:15 +0200

    create workflow for e2e tests. add test for apiconsumer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,66 @@
 // This configuration file sets up the paths and compilers so we can test our solidity samples
+import * as dotenv from 'dotenv';
 
-require("@nomiclabs/hardhat-waffle");
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-dependency-compiler';
 
-module.exports = {
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const accounts = process.env.MNEMONIC !== undefined ? { mnemonic: process.env.MNEMONIC } : [];
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+const config: HardhatUserConfig = {
   paths: {
-    root: "./",
-    sources: "./_includes/samples",
-    cache: "./.test/cache",
-    artifacts: "./.test/artifacts",
+    root: './',
+    sources: './_includes/samples',
+    cache: './.test/cache',
+    artifacts: './.test/artifacts',
   },
   solidity: {
     compilers: [
-      {version: "0.4.24"},
-      {version: "0.6.6"},
-      {version: "0.6.12"},
-      {version: "0.7.6"},
-      {version: "0.8.7"},
+      { version: '0.4.24' },
+      { version: '0.6.6' },
+      { version: '0.6.12' },
+      { version: '0.7.6' },
+      { version: '0.8.7' },
     ],
   },
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || '',
+      accounts,
+    },
+    kovan: {
+      url: process.env.KOVAN_URL || '',
+      accounts,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || '',
+  },
+  mocha: {
+    timeout: 300000,
+  },
+  typechain: {
+    outDir: './.test/typechain',
+    // externalArtifacts: ['./node_modules/@chainlink/contracts/abi/**/*.json'],
+  },
+  dependencyCompiler: {
+    paths: ['@chainlink/contracts/src/v0.4/interfaces/ERC677.sol'],
+  },
 };
+
+export default config;
---
+++ test/config.ts
@@ -0,0 +1,12 @@
+const config = {
+  kovan: {
+    linkAddress: '0xa36085F69e2889c224210F603D836748e7dC0088',
+    chainId: 42,
+  },
+  rinkeby: {
+    linkAddress: '0x01BE23585060835E02B77ef475b0Cc51aA1e0709',
+    chainId: 4,
+  },
+};
+
+export default config;
