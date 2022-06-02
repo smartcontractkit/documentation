b6e14c11e91c1b3b2ff8776277223ced49b27cef
pappas999 <harry@genesisblockchain.com.au>
2022-06-02 16:34:11 +0930

    fixed typo on feed address description

--- _includes/samples/Solana/PriceFeeds/off-chain-read.js
+++ _includes/samples/Solana/PriceFeeds/off-chain-read.js
@@ -8,7 +8,7 @@ async function main() {
 
     const CHAINLINK_FEED_ADDRESS="HgTtcbcmp5BeThax5AU8vg4VwK79qAvAKKFMs8txMLW6"
     const CHAINLINK_PROGRAM_ID = new anchor.web3.PublicKey("cjg3oHmg9uuPsP8D6g29NWvhySJkdYdAo9D25PRbKXJ");
-    const feedAddress = new anchor.web3.PublicKey(CHAINLINK_FEED_ADDRESS); //ETH-USD Devnet
+    const feedAddress = new anchor.web3.PublicKey(CHAINLINK_FEED_ADDRESS); //SOL-USD Devnet Feed
 
     //load the data feed account
     let dataFeed = await chainlink.OCR2Feed.load(CHAINLINK_PROGRAM_ID, provider);
