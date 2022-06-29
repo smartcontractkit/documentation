af94af548d2de31c50c9e97460e4841c2559732a
Dwight Lyle <dwightjl@gmail.com>
2022-06-29 09:26:39 -0600

    Minor fixes

--- _includes/samples/VRF/RandomNumberConsumer.sol
+++ _includes/samples/VRF/RandomNumberConsumer.sol
@@ -25,7 +25,7 @@ contract RandomNumberConsumer is VRFConsumerBase {
      *
      * Network: Rinkeby
      * Chainlink VRF Coordinator address: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
-     * LINK token address:                	0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+     * LINK token address: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709
      * Key Hash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
      */
     constructor()
