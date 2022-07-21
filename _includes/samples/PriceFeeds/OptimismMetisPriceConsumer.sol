b5024d47a1241b5fa608d56b9910ed6da9b06947
Dwight Lyle <dwightjl@gmail.com>
2022-07-21 01:55:10 -0600

    Update the L2 sequencer feed documentation; add Optimism and Metis

---
+++ _includes/samples/PriceFeeds/OptimismMetisPriceConsumer.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV2V3Interface.sol";
+
+/**
+ * Find information on LINK Token Contracts here: https://docs.chain.link/docs/link-token-contracts/
+ */
+
+contract PriceConsumerWithL2Sequencer {
+    // Identifier of the Sequencer offline flag on the Flags contract
+    AggregatorV2V3Interface internal priceFeed;
+    AggregatorV2V3Interface internal sequencerUptimeFeed;
+
+    uint256 private constant STALENESS_THRESHOLD = 1800;
+    uint256 private constant GRACE_PERIOD_TIME = 3600;
+
+    error StaleFeed();
+    error GracePeriodNotOver();
+
+    /**
+     * Network: Optimism
+     * Data Feed: BTC/USD
+     * Data Feed Proxy Address: 0xD702DD976Fb76Fffc2D3963D037dfDae5b04E593
+     * Sequencer Uptime Proxy Address: 0x371EAD81c9102C9BF4874A9075FFFf170F2Ee389
+     * For a list of available sequencer proxy addresses, see:
+     * https://docs.chain.link/docs/l2-sequencer-flag/
+     */
+    constructor() {
+        priceFeed = AggregatorV2V3Interface(0xD702DD976Fb76Fffc2D3963D037dfDae5b04E593);
+        sequencerUptimeFeed = AggregatorV2V3Interface(0x371EAD81c9102C9BF4874A9075FFFf170F2Ee389);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        if (!checkSequencerState()) {
+                // If the sequencer is down, do not perform any critical operations
+            revert("L2 sequencer down: Chainlink feeds are not being updated");
+        }
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    /**
+     * Check if the L2 sequencer is running
+     */
+    function checkSequencerState() public view returns (bool sequencerIsUp) {
+        (
+            /*uint80 roundId*/,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            /*uint80 answeredInRound*/
+        ) = sequencerUptimeFeed.latestRoundData();
+
+        // Feed should update every half an hour.  Consumers are free
+        // to define another threshold.
+        uint256 timeSinceLastUpdate = block.timestamp - updatedAt;
+        if (timeSinceLastUpdate > STALENESS_THRESHOLD) {
+            revert StaleFeed();
+        }
+
+        // Answer == 0: Sequencer is up
+        // Answer == 1: Sequencer is down
+        bool isSequencerUp = answer == 0; // 0 = up
+        if (isSequencerUp) {
+            uint256 timeSinceUp = block.timestamp - startedAt;
+            if (timeSinceUp <= GRACE_PERIOD_TIME) {
+                revert GracePeriodNotOver();
+            }
+        }
+        return isSequencerUp;
+    }
+}
