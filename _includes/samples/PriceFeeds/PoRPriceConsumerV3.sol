da272643e295f4983dd11a9d9c9e02f0482018aa
Dwight Lyle <dwightjl@gmail.com>
2022-09-25 18:59:18 -0700

    Add conceptual guide and update processing

---
+++ _includes/samples/PriceFeeds/PoRPriceConsumerV3.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Ethereum Mainnet
+     * Aggregator: WBTC PoR
+     * Address: 0xa81FE04086865e63E12dD3776978E49DEEa2ea4e
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(0xA39434A63A52E749F02807ae27335515BA4b07F7);
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
