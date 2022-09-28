17d53607769ca6d8e9114793c0b1cdaf9746a8ec
Fernando Montero <fernando.montero.perez@gmail.com>
2022-09-28 17:16:58 +0100

    added changefreq for sitemap (#76)

--- .astro/astro.config.ts
+++ .astro/astro.config.ts
@@ -1,28 +1,43 @@
-import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
-import react from "@astrojs/react";
-import { astroCallouts } from "./integrations/astro-callouts";
-import { solidityRemixCode } from "./integrations/solidity-remix";
-import { youtubeEmbed } from "./integrations/youtube-embed";
-import mdx from "@astrojs/mdx";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config"
+import preact from "@astrojs/preact"
+import react from "@astrojs/react"
+import { astroCallouts } from "./integrations/astro-callouts"
+import { solidityRemixCode } from "./integrations/solidity-remix"
+import { youtubeEmbed } from "./integrations/youtube-embed"
+import mdx from "@astrojs/mdx"
+import rehypeSlug from "rehype-slug"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import sitemap from "@astrojs/sitemap"
 
-import partytown from "@astrojs/partytown";
+import partytown from "@astrojs/partytown"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.chain.link",
   legacy: {
-    astroFlavoredMarkdown: true
+    astroFlavoredMarkdown: true,
   },
-  integrations: [preact(), react(), sitemap(), astroCallouts(), solidityRemixCode(), youtubeEmbed(), mdx(), partytown()],
+  integrations: [
+    preact(),
+    react(),
+    sitemap({ changefreq: "daily" }),
+    astroCallouts(),
+    solidityRemixCode(),
+    youtubeEmbed(),
+    mdx(),
+    partytown(),
+  ],
   markdown: {
-    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
-      behavior: "prepend"
-    }]],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+        },
+      ],
+    ],
     syntaxHighlight: "prism",
-    extendDefaultPlugins: true
-  }
-});
+    extendDefaultPlugins: true,
+  },
+})
