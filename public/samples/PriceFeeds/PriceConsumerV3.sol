7d1af0662c16bb15b035371fb04fc8fde0bdcc28
xeway <65366494+Xeway@users.noreply.github.com>
2023-01-06 20:52:33 +0100

    Updated Solidity Price Feeds example (PriceConsumerV3.sol)

--- public/samples/PriceFeeds/PriceConsumerV3.sol
+++ public/samples/PriceFeeds/PriceConsumerV3.sol
@@ -22,11 +22,11 @@ contract PriceConsumerV3 {
      */
     function getLatestPrice() public view returns (int) {
         (
-            ,
-            /*uint80 roundID*/ int price /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/,
-            ,
-            ,
-
+            /* uint80 roundID */,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
         ) = priceFeed.latestRoundData();
         return price;
     }
