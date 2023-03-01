df96e28bfc5e2b5b2e0971a1fb7405fe113bd828
Dwight Lyle <dwightjl@gmail.com>
2023-03-01 11:36:48 -0700

    Remove the .eleventy folder

--- .eleventy/_includes/snippets/Keepers/validationOfTriggers1.sol
+++
@@ -1,7 +0,0 @@
-function checkUpkeep(bytes calldata checkData) public view returns(bool, bytes memory) {
-  return (block.number > threshold, bytes(""));
-}
-
-function performUpkeep(bytes calldata performData) external {
-  doSomething(); // runs even if the checkUpkeep condition isn't satisfied
-}
