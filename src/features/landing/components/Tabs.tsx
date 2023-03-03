dcec10be057c3a6981eb8bbb3a209fc1691a5553
Dwight Lyle <dwightjl@gmail.com>
2023-03-03 12:10:04 -0700

    Remove the useState logic from tabs

--- src/features/landing/components/Tabs.tsx
+++ src/features/landing/components/Tabs.tsx
@@ -1,21 +1,16 @@
 /** @jsxImportSource preact */
-import { useState } from "preact/hooks"
 import { clsx } from "~/lib"
 import tabs from "./Tabs.module.css"
 import { ProductCard } from "./ProductCard"
 import { evmProducts } from "../data"
 
 export const Tabs = () => {
-  const [tabId] = useState("evm-products")
 
   return (
     <>
       <div class={tabs.tabContent}>
         <div
-          id="evm-products"
-          class={clsx(tabs.tabPane, {
-            [tabs.active]: tabId === "evm-products",
-          })}
+          class={clsx(tabs.tabPane, tabs.active)}
         >
           {evmProducts.map((props) => (
             <div class={tabs.tabElement}>
