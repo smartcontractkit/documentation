91262b241be6953916e06b05a15bf577e673c59b
thedriftofwords <crystal.gomes@gmail.com>
2023-03-09 14:08:41 -0500

    Another config update

--- astro.config.ts
+++ astro.config.ts
@@ -10,10 +10,7 @@ import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://docs.chain.link",
-  legacy: {
-    astroFlavoredMarkdown: true,
-  },
+  site: "https://docs.chain.link/",
   integrations: [
     preact({ compat: true }),
     sitemap({ changefreq: "daily" }),
