4c567e00eb8bba874b0b27d68bf22710f8407e00
Crystal Gomes <thedriftofwords@users.noreply.github.com>
2023-05-05 17:27:56 -0400

    Astro 2 (#1189)
    * Update package versions
    * Minor edits in Getting Started section
    * Deleted unused custom integrations
    * Update configs + minor node ops doc edits
    * Manually remove trailing slashes where we defined them
    * Set trailingSlash to false in Vercel
    * Exclude css files from linkcheck in new build location
    * Remove trailing slashes
    * Migrate last few files to mdx, upgrade typescript version
    * Construct canonicalURL without trailing slash
    * Enable gfm for tables to render correctly
    * Trim internal slashes - data feeds
    * Trim internal trailing slashes - nodes
    * Trim internal trailing slashes - getting started
    * Trim internal trailing slashes - automation
    * Trim internal trailing slashes - functions
    * Trim internal trailing slashes - resources etc
    * Trim internal trailing slashes - any-api, vrf
    
    ---------
    
    Co-authored-by: Dwight Lyle <dwightjl@gmail.com>

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from "astro/config"
 import preact from "@astrojs/preact"
-import react from "@astrojs/react"
-import { astroCallouts } from "./integrations/astro-callouts"
-import { solidityRemixCode } from "./integrations/solidity-remix"
-import { youtubeEmbed } from "./integrations/youtube-embed"
 import mdx from "@astrojs/mdx"
 import rehypeSlug from "rehype-slug"
 import rehypeAutolinkHeadings from "rehype-autolink-headings"
@@ -12,17 +8,8 @@ import sitemap from "@astrojs/sitemap"
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.chain.link",
-  legacy: {
-    astroFlavoredMarkdown: true,
-  },
-  integrations: [
-    preact({ compat: true }),
-    sitemap({ changefreq: "daily" }),
-    astroCallouts(),
-    solidityRemixCode(),
-    youtubeEmbed(),
-    mdx(),
-  ],
+  // trailingSlash: 'never',
+  integrations: [preact({ compat: true }), sitemap({ changefreq: "daily" }), mdx()],
   markdown: {
     drafts: true,
     rehypePlugins: [
@@ -35,6 +22,7 @@ export default defineConfig({
       ],
     ],
     syntaxHighlight: "prism",
-    extendDefaultPlugins: true,
+    smartypants: false,
+    gfm: true,
   },
 })
