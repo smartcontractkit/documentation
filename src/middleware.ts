c2252faf1bca7f06d9dacbf8287a5b873b318a3c
Fernando Montero <fernando.montero.perez@gmail.com>
2023-05-15 15:42:34 +0100

    add edge middleware

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import redirects from "./features/redirects/redirects.json"
+
+export function middleware(request: Request) {
+  const referrer = request.referrer
+  const url = new URL(request.url)
+  const pathname = url.pathname
+
+  const redirect = redirects.redirects.find((entry) => entry.source === pathname)
+
+  if (redirect) {
+    // You can also set request headers in NextResponse.rewrite
+    const finalUrl = new URL(redirect.destination)
+    finalUrl.searchParams.append("referrer", referrer)
+
+    return Response.redirect(redirect.destination)
+  }
+}
