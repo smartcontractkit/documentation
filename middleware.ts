b6366b2c27187541f18d2cbf8790b8d5c806563e
Fernando Montero <fernando.montero.perez@gmail.com>
2023-05-17 07:27:57 +0100

    add middleware redirect for one url

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,20 @@ export const config = {
   matcher: "/((?!api|static|.*\\..*|_astro|src).*)",
 }
 
+const testRedirections = [
+  {
+    source: "/docs/chainlink-keepers/introduction",
+    destination: "/chainlink-automation/introduction",
+    statuscode: 301,
+  },
+]
+
 export default function middleware(request: Request) {
   const url = new URL(request.url)
   const pathname = url.pathname
   const referrer = request.headers.get("referer")
 
-  const redirect = redirects.redirects.find((entry) => entry.source === pathname)
+  const redirect = testRedirections.find((entry) => entry.source === pathname)
 
   if (redirect) {
     const finalUrl = new URL(request.url)
