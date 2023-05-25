a2461444ba7096bda4a8538f5df92f3ed05f7f31
Fernando Montero <fernando.montero.perez@gmail.com>
2023-05-25 10:08:31 +0100

    enable redirects for all

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,17 @@
-import redirects from "./src/features/redirects/redirects.json"
+import redirectsFile from "./src/features/redirects/redirects.json"
 
 export const config = {
   matcher: "/((?!api|static|.*\\..*|_astro|src).*)",
 }
 
-const testRedirections = [
-  {
-    source: "/docs/chainlink-keepers/introduction",
-    destination: "/chainlink-automation/introduction",
-    statuscode: 301,
-  },
-]
+const { redirects } = redirectsFile
 
 export default function middleware(request: Request) {
   const url = new URL(request.url)
   const pathname = url.pathname
   const referrer = request.headers.get("referer")
 
-  const redirect = testRedirections.find((entry) => entry.source === pathname)
+  const redirect = redirects.find((entry) => entry.source === pathname)
 
   if (redirect) {
     const finalUrl = new URL(request.url)
