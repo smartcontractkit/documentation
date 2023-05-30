2779b71902e3c9d3bc152715b803a65794a6bf8c
thedriftofwords <crystal.gomes@gmail.com>
2023-05-30 14:13:37 -0400

    Remove commented out line

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,6 @@ import sitemap from "@astrojs/sitemap"
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.chain.link",
-  trailingSlash: "never",
   integrations: [
     preact({ compat: true }),
     sitemap({
