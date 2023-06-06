957e7d12ace568ac38da77d743aab7eaa5dbfbc3
Amine E <aelmanaa@users.noreply.github.com>
2023-06-06 22:54:47 +0100

    Bedrock notice update (#1330)
    * Bedrock notice update

--- src/features/feeds/components/FeedList.tsx
+++ src/features/feeds/components/FeedList.tsx
@@ -124,39 +124,20 @@ export const FeedList = ({
                       </a>
                     </h2>
                     {selectedChain === "optimism" && (
-                      <aside class="content danger astro-DUQFCLOB" aria-label="Optimism Bedrock Upgrade">
-                        <div class="icon astro-DUQFCLOB">
-                          <img
-                            src="/_astro/danger-icon.1d4263e7.svg"
-                            style="width: 1.5em;height: 1.5em;"
-                            alt="danger"
-                            class="astro-DUQFCLOB"
-                          ></img>
-                        </div>
+                      <aside class="content note astro-DUQFCLOB" aria-label="Optimism Bedrock Upgrade">
+                        <img
+                          src="/_astro/info-icon.ca56bc94.svg"
+                          style="width: 1.5em;height: 1.5em;"
+                          alt="note"
+                          class="astro-DUQFCLOB"
+                        ></img>
                         <section class="asideContent astro-DUQFCLOB">
                           <p class="title heading-100 astro-DUQFCLOB" aria-hidden="true">
                             Optimism Bedrock Upgrade
                           </p>
                           <p>
-                            Optimism Mainnet will{" "}
-                            <a
-                              href="https://community.optimism.io/docs/developers/bedrock/explainer/"
-                              target="_blank"
-                              rel="noopener"
-                            >
-                              migrate to Bedrock
-                            </a>{" "}
-                            on June 6th at 16:00 UTC. During this process, Optimism Mainnet will have an estimated 2-4
-                            hour downtime as the Optimism team performs the upgrade.
-                          </p>
-                          <p>
-                            <strong>
-                              PLEASE NOTE: During the downtime, please do not consume Chainlink Data Feeds or use
-                              Chainlink Automation services on Optimism Mainnet until we update you of the services
-                              availability. If you are actively consuming Chainlink Data Feeds or using Chainlink
-                              Automation on Optimism, we advise pausing your protocol operation in advance of the
-                              Bedrock upgrade. You can resume once Chainlink services are available for use.
-                            </strong>
+                            Optimism Bedrock mainnet feeds and Chainlink Automation services are now live and ready for
+                            consumption. Let us know if you experience any issues or have any questions.
                           </p>
                         </section>
                       </aside>
