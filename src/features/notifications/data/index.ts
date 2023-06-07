194ac9475ce36f0f7652479193ed1e3d0f61ccfc
Dwight Lyle <dwightjl@gmail.com>
2023-06-06 21:41:19 -0700

    Update banner

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -7,9 +7,9 @@ import { BannerContent } from "../components/HeaderBanner"
 // export const NotificationData: BannerContent | null = null
 
 export const NotificationData: BannerContent | null = {
-  description: "Attend SmartCon 2023 to explore the future of Web3.",
+  description: "Chainlink is collaborating with Swift and 12+ leading financial institutions.",
   type: "info",
-  linkText: "Sign up now.",
+  linkText: "Read the announcement.",
   linkUrl:
-    "https://smartcon.chain.link/?utm_medium=referral&utm_source=chainlink-docs&utm_campaign=smartcon-2023&agid=kgmdzq5cr7sn",
+    "https://www.swift.com/news-events/news/swift-explores-blockchain-interoperability-remove-friction-tokenised-asset-settlement",
 }
