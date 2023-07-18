d2dcbc6eefa874a90ee24efb99139650eb4bbd23
aelmanaa <aelmanaa@gmail.com>
2023-07-18 21:51:30 +0200

    helper to find nonchecksummed addresses

--- src/scripts/helper/find-nonchecksummed-addresses.ts
+++ src/scripts/helper/find-nonchecksummed-addresses.ts
@@ -3,10 +3,12 @@ import { ethers } from "ethers"
 import glob from "glob"
 import path from "path"
 
-const directoryPath = path.resolve(__dirname, "../../../src")
+const directoryPaths = [path.resolve(__dirname, "../../../src"), path.resolve(__dirname, "../../../public")]
+// List of files to ignore
+const ignoreFiles = [path.resolve(__dirname, "../../../public/search-index.json")]
 
 // helper function to check if a string is a valid Ethereum address
-const isAddress = (value: string) => {
+const getCorrectAddress = (value: string) => {
   try {
     return ethers.utils.getAddress(value)
   } catch {
@@ -19,29 +21,42 @@ const checkFile = async (filePath: string) => {
   const fileContent = await fs.readFile(filePath, "utf-8")
   const lines = fileContent.split("\n")
 
-  lines.forEach((line, index) => {
-    const words = line.split(" ")
+  const ethAddressRegex = /\b0x[a-fA-F0-9]{40}\b/gi
 
-    words.forEach((word) => {
-      if (isAddress(word) && ethers.utils.getAddress(word) !== word) {
-        console.log(`Improperly checksummed address found in file ${filePath} on line ${index + 1}: ${word}`)
-      }
-    })
+  lines.forEach((line, index) => {
+    const matches = line.match(ethAddressRegex)
+
+    if (matches) {
+      matches.forEach((match) => {
+        const correctAddress = getCorrectAddress(match)
+        if (correctAddress && correctAddress !== match) {
+          console.log(
+            `Improperly checksummed address found in file ${filePath} on line ${
+              index + 1
+            }: ${match} - correct checkSummed address is ${correctAddress}`
+          )
+        }
+      })
+    }
   })
 }
 
-// use glob to read all files in the directory (and subdirectories)
-glob.glob(directoryPath + "/**/*", (err, paths) => {
-  if (err) {
-    console.error("An error occurred while trying to read the files:", err)
-    return
-  }
-
-  // iterate over each path
-  paths.forEach((filepath) => {
-    // use fs.statSync() to check if the path is a file
-    if (statSync(filepath).isFile()) {
-      checkFile(filepath)
+directoryPaths.forEach((directoryPath) => {
+  // use glob to read all files in the directory (and subdirectories)
+  glob.glob(directoryPath + "/**/*", (err, paths) => {
+    if (err) {
+      console.error("An error occurred while trying to read the files:", err)
+      return
     }
+
+    // iterate over each path
+    paths.forEach((filepath) => {
+      // use fs.statSync() to check if the path is a file
+      if (statSync(filepath).isFile()) {
+        if (!ignoreFiles.includes(filepath)) {
+          checkFile(filepath)
+        }
+      }
+    })
   })
 })
