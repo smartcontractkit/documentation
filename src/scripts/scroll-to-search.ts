4c94bd05f87aa07045669470ed15fbbaee732f3f
simkasss <s.kastantinaviciute@gmail.com>
2023-07-24 21:02:02 +0300

    update placeholder

--- src/scripts/scroll-to-search.ts
+++ src/scripts/scroll-to-search.ts
@@ -2,11 +2,15 @@ export {}
 window.addEventListener("load", () => {
   const url = `${window.location.href}`
   const searchRegex = /search.*?(?==)/
-  const searchQuery = url.match(searchRegex)
-  if (searchQuery) {
-    const searchElement = document.getElementById(searchQuery[0])
+  const searchElementId = url.match(searchRegex)
+  const queryParams = new URLSearchParams(window.location.search)
+
+  if (searchElementId) {
+    const searchElement = document.getElementById(searchElementId[0]) as HTMLInputElement
     if (searchElement) {
       searchElement.scrollIntoView({ behavior: "smooth" })
+      const input = queryParams.get(searchElementId[0])
+      searchElement.placeholder = input
     }
   }
 })
