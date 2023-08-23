092e64c15fad11788d3ef57a34b450d47b535ad8
thedriftofwords <crystal.gomes@gmail.com>
2023-08-23 16:47:27 -0400

    Fresh screenshots, sample contract, other edits

---
+++ public/samples/Automation/Counter.sol
@@ -0,0 +1,31 @@
+/**
+ *Submitted for verification at Etherscan.io on 2023-08-23
+ */
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+contract Counter {
+    /**
+     * Public counter variable
+     */
+    uint public counter;
+
+    constructor() {
+        counter = 0;
+    }
+
+    function updateCounter(uint updateAmount) public {
+        counter += updateAmount;
+    }
+
+    function getCounter() public view returns (uint) {
+        return counter;
+    }
+}
