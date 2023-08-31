61959b2367f292800c41c26afab50d2c06e90426
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-08-31 15:04:11 -0400

    Added arrow for mobile TOC

--- src/components/RightSidebar/TableOfContents/index.tsx
+++ src/components/RightSidebar/TableOfContents/index.tsx
@@ -43,6 +43,22 @@ const TableOfContents: FunctionalComponent = () => {
               ${$currentId === h.slug ? ` ${styles.active}` : ""}`}
             >
               {h.text}
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M16 12.25H7"
+                  stroke="#375BD2"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M12.5 8.25L16.25 12L12.5 15.75"
+                  stroke="#375BD2"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
             </a>
           </li>
         ))}
