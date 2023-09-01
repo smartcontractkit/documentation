1845b4859b5dcbb2ad5a5789e9d4a68a9bd20627
Dwight Lyle <dwightjl@gmail.com>
2023-08-31 18:59:17 -0600

    Update RDD URL and Link Token Contracts

--- src/features/data/chains.ts
+++ src/features/data/chains.ts
@@ -341,8 +341,7 @@ export const CHAINS: Chain[] = [
         name: "Scroll Sepolia testnet",
         explorerUrl: "https://sepolia-blockscout.scroll.io/address/%s",
         networkType: "testnet",
-        rddUrl:
-          "https://reference-data-directory-70fsby0i4-chainlinklabs.vercel.app/feeds-ethereum-testnet-sepolia-scroll-1.json",
+        rddUrl: "https://reference-data-directory.vercel.app/feeds-ethereum-testnet-sepolia-scroll-1.json",
       },
     ],
   },
