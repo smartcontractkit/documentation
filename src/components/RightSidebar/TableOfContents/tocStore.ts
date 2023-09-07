bf44bd40111419040ee4df64416c37974b25c7eb
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-09-06 21:20:05 -0400

    Fix anchors not scrolling correctly initially

--- src/components/RightSidebar/TableOfContents/tocStore.ts
+++ src/components/RightSidebar/TableOfContents/tocStore.ts
@@ -1,16 +1,6 @@
-// store/users.ts
 import { atom } from "nanostores"
 
 let updateCounter = 0
 
 export const shouldUpdateToc = atom<number | undefined>()
-export function updateTableOfContents() {
-  shouldUpdateToc.set(++updateCounter)
-
-  // Handle anchors after TOC loads
-  if (window.location.hash) {
-    const id = decodeURI(window.location.hash.split("#")[1])
-    const scrollTo = document.getElementById(id)
-    scrollTo?.scrollIntoView()
-  }
-}
+export const updateTableOfContents = () => shouldUpdateToc.set(++updateCounter)
