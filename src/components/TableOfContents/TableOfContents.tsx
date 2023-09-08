22a741c6aa9d242607a00cb6a8b0460ace4f12b0
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-09-08 15:12:23 -0400

    Spacing and key on TableOfContents

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -21,10 +21,12 @@ const TableOfContents: FunctionalComponent<{
     // Only get top-level headers, don't get nested component headers
     const elements = document.querySelectorAll("article section > :where(h1, h2, h3, h4)")
     const newHeadings: MarkdownHeading[] = []
+
     // If there are no new headers in the DOM, return
     if (elements.length === headings.length) {
       return
     }
+
     elements.forEach((e) => {
       // Check for nextElementSibling, if there's no content
       // following the header (i.e. empty overview)
@@ -37,6 +39,7 @@ const TableOfContents: FunctionalComponent<{
         })
       }
     })
+
     setHeadings(newHeadings)
   }, [$shouldUpdateToc])
 
@@ -45,7 +48,7 @@ const TableOfContents: FunctionalComponent<{
       <ContentObserver headings={headings} shouldUpdate={$shouldUpdateToc} />
       <ul>
         {headings.map((h) => (
-          <li onClick={onNavigate}>
+          <li key={h.slug} onClick={onNavigate}>
             <a
               href={`#${h.slug}`}
               className={`${styles.headerLink}${h.depth && h.depth > 2 ? ` ${styles[`depth-${h.depth}`]}` : ""}
