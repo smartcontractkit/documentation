9e71998a4c72ea4ba37a4a85b407b7a6a07e7044
thedriftofwords <crystal.gomes@gmail.com>
2023-09-11 16:57:05 -0400

    Update Counter.sol

--- public/samples/Automation/Counter.sol
+++ public/samples/Automation/Counter.sol
@@ -1,9 +1,7 @@
-/**
- *Submitted for verification at Etherscan.io on 2023-08-23
- */
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
@@ -11,21 +9,34 @@ pragma solidity ^0.8.7;
  * DO NOT USE THIS CODE IN PRODUCTION.
  */
 
-contract Counter {
+contract Counter is Ownable {
     /**
      * Public counter variable
      */
     uint public counter;
 
+    // the address of the Automation registry
+    address public s_automationRegistryAddress;
+
+    error OnlyAutomationRegistry();
+
+    // Hardcoded for Sepolia:
+    // s_automationRegistryAddress = 0xE16Df59B887e3Caa439E0b29B42bA2e7976FD8b2
     constructor() {
         counter = 0;
+        s_automationRegistryAddress = 0xE16Df59B887e3Caa439E0b29B42bA2e7976FD8b2;
     }
 
-    function updateCounter(uint updateAmount) public {
+    function updateCounter(
+        uint updateAmount
+    ) public onlyOwner onlyAutomationRegistry {
         counter += updateAmount;
     }
 
-    function getCounter() public view returns (uint) {
-        return counter;
+    modifier onlyAutomationRegistry() {
+        if (msg.sender != s_automationRegistryAddress) {
+            revert OnlyAutomationRegistry();
+        }
+        _;
     }
 }
