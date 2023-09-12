51c3880d41e7942b91f25ce4bdc96b39967e2dfd
thedriftofwords <crystal.gomes@gmail.com>
2023-09-11 23:49:56 -0400

    Address review comments and revert to simple contract

--- public/samples/Automation/Counter.sol
+++ public/samples/Automation/Counter.sol
@@ -1,42 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 /**
  * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
  * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
  * DO NOT USE THIS CODE IN PRODUCTION.
  */
 
-contract Counter is Ownable {
+contract Counter {
     /**
      * Public counter variable
      */
     uint public counter;
 
-    // the address of the Automation registry
-    address public s_automationRegistryAddress;
-
-    error OnlyAutomationRegistry();
-
-    // Hardcoded for Sepolia:
-    // s_automationRegistryAddress = 0xE16Df59B887e3Caa439E0b29B42bA2e7976FD8b2
     constructor() {
         counter = 0;
-        s_automationRegistryAddress = 0xE16Df59B887e3Caa439E0b29B42bA2e7976FD8b2;
     }
 
-    function updateCounter(
-        uint updateAmount
-    ) public onlyOwner onlyAutomationRegistry {
+    function updateCounter(uint updateAmount) external {
         counter += updateAmount;
-    }
-
-    modifier onlyAutomationRegistry() {
-        if (msg.sender != s_automationRegistryAddress) {
-            revert OnlyAutomationRegistry();
-        }
-        _;
     }
 }
