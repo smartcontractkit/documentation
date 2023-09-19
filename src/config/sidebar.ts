1401d881bb145863f86a8e7595a8ba13db040319
Dwight Lyle <dwightjl@gmail.com>
2023-09-18 19:39:36 -0600

    Navigation changes, URL updates, and redirects

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -144,7 +144,7 @@ export const SIDEBAR: Partial<Record<Sections, SectionEntry[]>> = {
           children: [
             {
               title: "Deploy Your First Smart Contract",
-              url: "getting-started/deploy-your-first-contract?parent=dataFeeds",
+              url: "quickstarts/deploy-your-first-contract?parent=dataFeeds",
             },
           ],
         },
@@ -309,7 +309,7 @@ export const SIDEBAR: Partial<Record<Sections, SectionEntry[]>> = {
           children: [
             {
               title: "Deploy Your First Smart Contract",
-              url: "getting-started/deploy-your-first-contract?parent=automation",
+              url: "quickstarts/deploy-your-first-contract?parent=automation",
             },
           ],
         },
@@ -479,7 +479,7 @@ export const SIDEBAR: Partial<Record<Sections, SectionEntry[]>> = {
           children: [
             {
               title: "Deploy Your First Smart Contract",
-              url: "getting-started/deploy-your-first-contract?parent=chainlinkFunctions",
+              url: "quickstarts/deploy-your-first-contract?parent=chainlinkFunctions",
             },
           ],
         },
@@ -639,7 +639,7 @@ export const SIDEBAR: Partial<Record<Sections, SectionEntry[]>> = {
           children: [
             {
               title: "Deploy Your First Smart Contract",
-              url: "getting-started/deploy-your-first-contract?parent=vrf",
+              url: "quickstarts/deploy-your-first-contract?parent=vrf",
             },
           ],
         },
@@ -805,7 +805,7 @@ export const SIDEBAR: Partial<Record<Sections, SectionEntry[]>> = {
           children: [
             {
               title: "Deploy Your First Smart Contract",
-              url: "getting-started/deploy-your-first-contract?parent=ccip",
+              url: "quickstarts/deploy-your-first-contract?parent=ccip",
             },
           ],
         },
@@ -1112,7 +1112,7 @@ export const SIDEBAR: Partial<Record<Sections, SectionEntry[]>> = {
           children: [
             {
               title: "Deploy Your First Smart Contract",
-              url: "getting-started/deploy-your-first-contract",
+              url: "quickstarts/deploy-your-first-contract",
             },
           ],
         },
