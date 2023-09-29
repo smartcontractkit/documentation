40b99c95f7acc2b52e496b1453f0dcf0961e0cbe
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-09-29 19:54:29 -0400

    Scroll adjustments

--- src/components/Header/useNavBar/useNavBar.ts
+++
@@ -1,7 +0,0 @@
-import { useStore } from "@nanostores/react"
-import { navBarInfo, NavBarInfo } from "./navBarStore"
-
-export const useNavBar = () => ({
-  $navBarInfo: useStore(navBarInfo),
-  setNavBarInfo: (info: NavBarInfo) => navBarInfo.set(info),
-})
