2fa6bc84e8916b39be6f48fe10a3254eb3795112
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-09-29 14:08:50 -0400

    Conditionally render search

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -1,6 +1,6 @@
 import { NavBar as Nav } from "@chainlink/components"
 import { Search } from "./AlgoSearch/Search"
 
-export const NavBar = ({ path }: { path: string }) => {
-  return <Nav app="Docs" path={path} searchTrigger={<Search />} />
+export const NavBar = ({ path, showSearch = true }: { path: string; showSearch?: boolean }) => {
+  return <Nav app="Docs" path={path} searchTrigger={showSearch ? <Search /> : undefined} />
 }
