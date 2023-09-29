a2a43e10ef1d001a88fc777e3a7a9958c74087d3
Dwight Lyle <dwightjl@gmail.com>
2023-09-29 13:22:22 -0600

    Restore nav

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -34,6 +34,11 @@ export const MENU: MenuItems = {
       section: "vrf",
     },
     {
+      text: "Data-Streams",
+      link: "/data-streams",
+      section: "dataStreams",
+    },
+    {
       text: "Nodes",
       link: "/chainlink-nodes",
       section: "nodeOperator",
