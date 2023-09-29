d9588bb8b2b97a6449c7a8e555bf8dd4103e2f12
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-09-29 19:10:34 -0400

    shifting top with navbar

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -4,16 +4,25 @@ import { Search } from "./AlgoSearch/Search"
 import { useScrollDirection } from "@chainlink/components/src/NavBar/useScrollDirection"
 import { useNavBar } from "./useNavBar/useNavBar"
 import { ScrollDirection } from "./useNavBar/navBarStore"
+import styles from "./scroll.module.css"
 
 export const NavBar = ({ path, showSearch = true }: { path: string; showSearch?: boolean }) => {
   const scrollDirection = useScrollDirection() as ScrollDirection
   const navRef = React.useRef(null)
 
-  const { setNavBarInfo, $navBarInfo } = useNavBar()
+  // TODO: May not need useNavBar after all
+  const { setNavBarInfo } = useNavBar()
 
   React.useEffect(() => {
     if (navRef.current) {
       const height = (navRef.current as HTMLElement).clientHeight
+      const elements = document.body.querySelectorAll("[data-sticky]")
+      elements.forEach((e: HTMLElement) => {
+        if (!e.classList.contains(styles.animateTop)) {
+          e.classList.add(styles.animateTop)
+        }
+        e.style.top = `${scrollDirection === "up" ? height : 0}px`
+      })
       setNavBarInfo({ scrollDirection, height })
     }
   }, [scrollDirection])
