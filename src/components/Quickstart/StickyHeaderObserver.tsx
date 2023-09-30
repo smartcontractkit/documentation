4ce6ccbfd5f294d0f6bde7760bb8989b3559484c
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-09-30 15:55:28 -0400

    Refined StickyHeaderObserver

--- src/components/Quickstart/StickyHeaderObserver.tsx
+++ src/components/Quickstart/StickyHeaderObserver.tsx
@@ -12,54 +12,96 @@ const StickyHeaderObserver: React.FC = () => {
   const navBarInfo = React.useRef<NavBarInfo>()
   navBarInfo.current = $navBarInfo
 
+  const ref = React.useRef<HTMLDivElement>(null)
+
   React.useEffect(() => {
+    const article = document.getElementById("article")
     const height = navBarInfo.current?.height
-    if (!height) return
-    const observerCallback: IntersectionObserverCallback = (entries, observer) => {
+    if (!article || !height || !ref.current) {
+      return
+    }
+
+    // Allows us to extend the bar to the gutter padding
+    const gridMain = document.getElementById("grid-main")
+    if (gridMain) {
+      ref.current.style.width = `${gridMain.clientWidth}px`
+      ref.current.style.transform = "translateX(calc(-1 * var(--gutter)))"
+    } else {
+      ref.current.style.width = `${article.clientWidth}px`
+    }
+
+    const articleObserverCallback: IntersectionObserverCallback = (entries, observer) => {
+      if (!ref.current) return
+      const scroll = navBarInfo.current?.scrollDirection
+      if (
+        (scroll === "up" && observer.rootMargin !== rootMarginDown) ||
+        (scroll === "down" && observer.rootMargin === rootMarginDown)
+      ) {
+        const article = entries[0]
+        ref.current.hidden = !article.isIntersecting
+      }
+    }
+
+    const headerObserverCallback: IntersectionObserverCallback = (entries, observer) => {
+      if (!ref.current) return
       const scroll = navBarInfo.current?.scrollDirection
       if (
         (scroll === "up" && observer.rootMargin !== rootMarginDown) ||
         (scroll === "down" && observer.rootMargin === rootMarginDown)
       ) {
         for (const entry of entries) {
-          const { isIntersecting, target } = entry
-          if (isIntersecting) {
-            target.classList.add(styles.background)
-          } else {
-            target.classList.remove(styles.background)
+          if (entry.isIntersecting) {
+            ref.current.style.height = `${entry.target.clientHeight}px`
           }
         }
       }
     }
 
-    // Add background to currently sticky header
-    const stickyHeaders = document.body.querySelectorAll("article > section > h2")
     let topUp = "0%"
     let bottomUp = "-94%"
     if (window?.innerHeight) {
       topUp = `-${height}px`
-      bottomUp = `-${window.innerHeight - height}px`
+      bottomUp = `-${window.innerHeight - height - 2}px`
     }
-    const stickyHeaderObserverUp = new IntersectionObserver(observerCallback, {
+
+    const articleObserverUp = new IntersectionObserver(articleObserverCallback, {
       rootMargin: `${topUp} 0% ${bottomUp} 0%`,
     })
-
-    const stickyHeaderObserverDown = new IntersectionObserver(observerCallback, {
+    const articleObserverDown = new IntersectionObserver(articleObserverCallback, {
       rootMargin: rootMarginDown,
     })
+    articleObserverUp.observe(article)
+    articleObserverDown.observe(article)
 
+    const headerObserverUp = new IntersectionObserver(headerObserverCallback, {
+      rootMargin: `${topUp} 0% ${bottomUp} 0%`,
+    })
+    const headerObserverDown = new IntersectionObserver(headerObserverCallback, {
+      rootMargin: rootMarginDown,
+    })
+    const stickyHeaders = document.body.querySelectorAll("article [data-sticky]")
     stickyHeaders.forEach((h) => {
-      stickyHeaderObserverUp.observe(h)
-      stickyHeaderObserverDown.observe(h)
+      headerObserverUp.observe(h)
+      headerObserverDown.observe(h)
     })
 
     return () => {
-      stickyHeaderObserverUp.disconnect()
-      stickyHeaderObserverDown.disconnect()
+      articleObserverUp.disconnect()
+      articleObserverDown.disconnect()
+      headerObserverUp.disconnect()
+      headerObserverDown.disconnect()
     }
   }, [$navBarInfo.height])
 
-  return null
+  React.useEffect(() => {
+    const { height, scrollDirection } = $navBarInfo
+    if (!height || !ref.current) {
+      return
+    }
+    ref.current.style.top = `${scrollDirection === "up" ? height : 0}px`
+  }, [$navBarInfo.scrollDirection, $navBarInfo.height])
+
+  return <div ref={ref} className={styles.stickyBar} />
 }
 
 export default StickyHeaderObserver
