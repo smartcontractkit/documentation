9e72a5b00fc522767a9f2b618b32fb00618b7dea
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-10-12 19:17:38 -0400

    Adjusted script for prepArticleContent

--- src/scripts/prepArticleContent.ts
+++ src/scripts/prepArticleContent.ts
@@ -1,9 +1,10 @@
 import GithubSlugger from "github-slugger"
 
-const parentMap: Record<string, string[]> = {
-  H1: ["H3", "H4", "H5", "H6"],
-  H2: ["H3", "H4", "H5", "H6"],
-  H3: ["H4", "H5", "H6"],
+const dontWrapMap: Record<string, string[]> = {
+  H1: ["H1", "H2"],
+  H2: ["H1", "H2"],
+  H3: ["H1", "H2", "H3"],
+  H4: ["H1", "H2", "H3", "H4"],
 }
 
 /**
@@ -15,15 +16,14 @@ const wrapHeader = (start: Element) => {
   const elements: Element[] = []
   elements.push(start)
   const depth = start.nodeName
-  const isParentTo = parentMap[depth] ?? []
+  const dontWrap = dontWrapMap[depth] ?? ["H1", "H2", "H3", "H4", "H5", "H6"]
   let next = start.nextElementSibling
   while (
-    next && // Just bear with me
+    next &&
+    !dontWrap.includes(next.nodeName) && // Just bear with me
     (next.nodeName !== "ASTRO-ISLAND" || // Not an island
       !next.hasChildNodes() || // Island doesn't have HTML content
-      next.children[0].nodeName !== "SECTION" || // First child of the island is not a section
-      !next.children[0].id) && // Section does not have an id
-    (!next.id || isParentTo.includes(next.nodeName))
+      next.children[0].nodeName !== "SECTION") // First child of the island is not a section
   ) {
     elements.push(next)
     next = next.nextElementSibling
@@ -49,11 +49,8 @@ const wrapATF = () => {
   let next = start.nextElementSibling
   while (
     next &&
-    (next.nodeName !== "ASTRO-ISLAND" ||
-      !next.hasChildNodes() ||
-      next.children[0].nodeName !== "SECTION" ||
-      !next.children[0].id) &&
-    (!next.id || !["SECTION"].includes(next.nodeName))
+    next.nodeName !== "SECTION" &&
+    (next.nodeName !== "ASTRO-ISLAND" || !next.hasChildNodes() || next.children[0].nodeName !== "SECTION")
   ) {
     elements.push(next)
     next = next.nextElementSibling
@@ -62,24 +59,21 @@ const wrapATF = () => {
   elements.forEach((e) => wrapper.appendChild(e))
 }
 
-const attachAnchor = (e: Element) => {
-  const anchor = document.createElement("a")
-  anchor.href = `#${e.id}`
-  anchor.textContent = e.textContent
-  e.replaceChildren(anchor)
-}
-
+// TODO: Consider also preparing top-level astro-islands, replacing SectionWrapper component
 /**
  * Performs all transformations on top-level article headers
  *
  * Assigns an id, creates an anchor, makes h1 and h2 responsive to data-sticky, and wraps the header and following content in a section tag
  *
- * Previously this was done by rehype plugins, but they are unreliable as they only work for markdown
+ * This picks up any headers not already modified by rehypeSlug and rehypeAutolinkHeadings (anything besides markdown content)
  */
 export const prepareHeaders = (aboveTheFold?: boolean) => {
   // Only get direct descendants of the article content
   // Grabs mdx headers without getting nested headers in components
-  const headers = document.body.querySelectorAll("#article > :where(h1, h2, h3, h4)")
+  const headers = document.body.querySelectorAll(
+    "#article > :where(h1, h2, h3, h4), #article > astro-island > :where(h1, h2, h3, h4)"
+  )
+  console.log(headers)
   const slugger = new GithubSlugger()
   headers.forEach((header) => {
     if (!header.textContent) {
@@ -88,8 +82,17 @@ export const prepareHeaders = (aboveTheFold?: boolean) => {
     if (["H1", "H2"].includes(header.nodeName)) {
       header.setAttribute("data-sticky", "")
     }
-    header.id = slugger.slug(header.textContent)
-    attachAnchor(header)
+    if (!header.id) {
+      header.id = slugger.slug(header.textContent)
+      if (header.firstChild?.nodeName !== "A") {
+        const anchor = document.createElement("a")
+        anchor.href = `#${header.id}`
+        anchor.textContent = header.textContent
+        header.replaceChildren(anchor)
+      }
+    }
+  })
+  headers.forEach((header) => {
     wrapHeader(header)
   })
 
