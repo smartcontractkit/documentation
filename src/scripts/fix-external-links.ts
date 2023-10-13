bec174f784679c25ce25d7d3ad1e91168417957d
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-10-13 15:45:58 -0400

    Dont open dev.chain.link links in a new tab

---
+++ src/scripts/fix-external-links.ts
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.links
+  for (let i = 0, linksLength = links.length; i < linksLength; i++) {
+    if (!links[i].href.startsWith("javascript:")) {
+      if (![window.location.hostname, "dev.chain.link"].includes(links[i].hostname)) {
+        links[i].target = "_blank"
+        links[i].relList.add("noopener")
+      }
+    }
+  }
+})
