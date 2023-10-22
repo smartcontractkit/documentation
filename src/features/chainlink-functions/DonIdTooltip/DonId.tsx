195533248995f5ae086b3826154945812a8bf0ad
aelmanaa <aelmanaa@gmail.com>
2023-10-22 19:14:36 +0200

    add DON ID in bytes32

--- src/features/chainlink-functions/DonIdTooltip/DonId.tsx
+++ src/features/chainlink-functions/DonIdTooltip/DonId.tsx
@@ -1,11 +1,19 @@
-/** @jsxImportSource preact */
 import { Tooltip } from "@chainlink/components"
 
 export const DonId = () => {
+  const containerStyle = {
+    display: "flex",
+    alignItems: "center",
+  }
+
+  const textStyle = {
+    marginRight: "0.25em", // using viewport width units for responsive margin
+  }
+
   return (
     <div>
-      <span>
-        <span>DON ID</span>
+      <span style={containerStyle}>
+        <span style={textStyle}>DON ID</span>
         <Tooltip
           tip={
             "The DON ID string is used for the Functions Toolkit NPM package, while the bytes32 hex is used for onchain DON calls."
