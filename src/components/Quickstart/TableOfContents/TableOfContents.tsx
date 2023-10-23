74959c52aec3fcaba92e43d5317b7f0615c562ae
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-10-23 15:31:54 -0400

    Refactored useCurrentIds and ContentObserver

--- src/components/Quickstart/TableOfContents/TableOfContents.tsx
+++ src/components/Quickstart/TableOfContents/TableOfContents.tsx
@@ -3,9 +3,9 @@ import type { FunctionalComponent } from "preact"
 import { useEffect, useState } from "preact/hooks"
 import { MarkdownHeading } from "astro"
 import styles from "./tableOfContents.module.css"
-import { useCurrentIds } from "~/hooks/currentIds/useCurrentIds"
 import { useStore } from "@nanostores/preact"
 import { shouldUpdateToc } from "./tocStore"
+import { clsx } from "~/lib"
 
 type HeaderWrapperClass = "header-wrapper-2" | "header-wrapper-3"
 const wrapperDepthMap: Record<HeaderWrapperClass, number> = {
@@ -17,9 +17,9 @@ const TableOfContents: FunctionalComponent<{
   initialHeadings: MarkdownHeading[]
 }> = ({ initialHeadings }) => {
   const $shouldUpdateToc = useStore(shouldUpdateToc)
-  const { $currentIds, setCurrentIds } = useCurrentIds()
 
   const [headings, setHeadings] = useState<MarkdownHeading[]>(initialHeadings)
+  const [currentIds, setCurrentIds] = useState<Record<string, boolean>>({})
 
   useEffect(() => {
     // Only get top-level headers, don't get nested component headers
@@ -48,12 +48,15 @@ const TableOfContents: FunctionalComponent<{
 
   useEffect(() => {
     const observerCallback: IntersectionObserverCallback = (entries) => {
-      const intersectingElementMap: Record<string, boolean> = $currentIds
-      for (const entry of entries) {
-        const { isIntersecting, target } = entry
-        intersectingElementMap[target.id] = isIntersecting
-      }
-      setCurrentIds({ ...intersectingElementMap })
+      setCurrentIds((currentIds) => {
+        const newIds: Record<string, boolean> = { ...currentIds }
+        for (const entry of entries) {
+          const { isIntersecting, target } = entry
+          const { id } = target
+          newIds[id] = isIntersecting
+        }
+        return newIds
+      })
     }
 
     const elementObserver = new IntersectionObserver(observerCallback, {
@@ -74,33 +77,36 @@ const TableOfContents: FunctionalComponent<{
       <h2 className={styles.heading}>On this page</h2>
       <ul>
         {headings &&
-          headings.map((h) => (
-            <li key={h.slug}>
-              <a
-                href={`#${h.slug}`}
-                className={`${styles.headerLink}${h.depth && h.depth > 2 ? ` ${styles[`depth-${h.depth}`]}` : ""}
-                ${$currentIds[h.slug] ? ` ${styles.active}` : ""}`}
-              >
-                {h.text}
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                  <path
-                    d="M16 12.25H7"
-                    stroke="#375BD2"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M12.5 8.25L16.25 12L12.5 15.75"
-                    stroke="#375BD2"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </a>
-            </li>
-          ))}
+          headings.map((h) => {
+            const className = clsx(
+              styles.headerLink,
+              h.depth > 2 && styles[`depth-${h.depth}`],
+              currentIds[h.slug] && styles.active
+            )
+            return (
+              <li key={h.slug}>
+                <a href={`#${h.slug}`} className={className}>
+                  {h.text}
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path
+                      d="M16 12.25H7"
+                      stroke="#375BD2"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M12.5 8.25L16.25 12L12.5 15.75"
+                      stroke="#375BD2"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </a>
+              </li>
+            )
+          })}
       </ul>
     </nav>
   )
--- src/components/StickyHeader/StickyHeader.tsx
+++ src/components/StickyHeader/StickyHeader.tsx
@@ -5,6 +5,7 @@ import { MarkdownHeading } from "astro"
 import styles from "./stickyHeader.module.css"
 import { useStickyHeader } from "~/hooks/stickyHeader/useStickyHeader"
 import TableOfContents from "~/components/TableOfContents/TableOfContents"
+import { clsx } from "~/lib"
 
 const StickyHeader: FunctionalComponent<{
   initialHeadings: MarkdownHeading[]
@@ -13,6 +14,8 @@ const StickyHeader: FunctionalComponent<{
   const expandButtonRef = useRef(null)
   const { $stickyHeader } = useStickyHeader()
   const hidden = !$stickyHeader
+
+  const buttonClassName = clsx("secondary", expanded && styles.active)
 
   return (
     <div
@@ -28,10 +31,10 @@ const StickyHeader: FunctionalComponent<{
       onClickCapture={(e) => setExpanded(e.target === expandButtonRef.current ? !expanded : false)}
     >
       <div hidden={!expanded || hidden} className={styles.tocWrapper}>
-        <TableOfContents initialHeadings={initialHeadings} />
+        <TableOfContents initialHeadings={initialHeadings} updateSticky />
       </div>
       <div className={styles.heading}>
-        <button ref={expandButtonRef} className={`secondary${expanded ? ` ${styles.active}` : ""}`}>
+        <button ref={expandButtonRef} className={buttonClassName}>
           On this page
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
             <path d="M8.70711 12.9498C8.31658 13.3403 7.68342 13.3403 7.29289 12.9498C6.90237 12.5592 6.90237 11.9261 7.29289 11.5355L11.5355 7.2929C11.9261 6.90238 12.5592 6.90238 12.9497 7.2929C13.3403 7.68342 13.3403 8.31659 12.9497 8.70711L8.70711 12.9498Z" />
