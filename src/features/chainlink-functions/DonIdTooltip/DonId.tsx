95c6b04127b9412f2db40c7b3bb2b45d1608eea7
aelmanaa <aelmanaa@gmail.com>
2023-10-24 11:28:05 +0200

    use generic tooltip

--- src/features/chainlink-functions/DonIdTooltip/DonId.tsx
+++ src/features/chainlink-functions/DonIdTooltip/DonId.tsx
@@ -1,27 +1,13 @@
-import { Tooltip } from "@chainlink/components"
+import { Tooltip } from "@features/common/Tooltip"
 
 export const DonId = () => {
-  const containerStyle = {
-    display: "flex",
-    alignItems: "center",
-  }
-
-  const textStyle = {
-    marginRight: "0.25em", // using viewport width units for responsive margin
-  }
-
   return (
-    <div>
-      <span style={containerStyle}>
-        <span style={textStyle}>DON ID</span>
-        <Tooltip
-          tip={
-            "The DON ID string is used for the Functions Toolkit NPM package, while the bytes32 hex is used for onchain DON calls."
-          }
-        >
-          <img src="https://smartcontract.imgix.net/icons/info.svg?auto=compress%2Cformat" width={12} height={12} />
-        </Tooltip>
-      </span>
-    </div>
+    <Tooltip
+      tip={
+        "The DON ID string is used for the Functions Toolkit NPM package, while the bytes32 hex is used for onchain DON calls."
+      }
+      label={"DON ID"}
+      imgURL={"https://smartcontract.imgix.net/icons/info.svg?auto=compress%2Cformat"}
+    ></Tooltip>
   )
 }
