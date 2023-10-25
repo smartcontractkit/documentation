e3577acd3203905b74ad961edbeb1c089dc98808
Ryan Nelson <ryan.nelson@smartcontract.com>
2023-10-25 14:35:53 -0400

    Replaced useCurrentIds hook with internal logic for TOC

--- src/components/TableOfContents/TableOfContents.tsx
+++ src/components/TableOfContents/TableOfContents.tsx
@@ -3,18 +3,20 @@ import type { FunctionalComponent } from "preact"
 import { useEffect, useState } from "preact/hooks"
 import { MarkdownHeading } from "astro"
 import styles from "./tableOfContents.module.css"
-import { useCurrentIds } from "~/hooks/currentIds/useCurrentIds"
-import { useStore } from "@nanostores/preact"
 import { shouldUpdateToc } from "./tocStore"
-import { ContentObserver } from "~/components/PageContent/ContentObserver"
+import { clsx } from "~/lib"
+import { useStore } from "@nanostores/preact"
+import { useStickyHeader } from "~/hooks/stickyHeader/useStickyHeader"
 
 const TableOfContents: FunctionalComponent<{
   initialHeadings: MarkdownHeading[]
-}> = ({ initialHeadings }) => {
+  updateSticky?: boolean
+}> = ({ initialHeadings, updateSticky }) => {
   const $shouldUpdateToc = useStore(shouldUpdateToc)
-  const { $currentIds } = useCurrentIds()
+  const { setStickyHeader } = useStickyHeader()
 
   const [headings, setHeadings] = useState<MarkdownHeading[]>(initialHeadings)
+  const [currentIds, setCurrentIds] = useState<Record<string, boolean>>({})
 
   useEffect(() => {
     // Only get top-level headers, don't get nested component headers
@@ -37,21 +39,69 @@ const TableOfContents: FunctionalComponent<{
     setHeadings(newHeadings)
   }, [$shouldUpdateToc])
 
+  useEffect(() => {
+    const observerCallback: IntersectionObserverCallback = (entries) => {
+      setCurrentIds((currentIds) => {
+        const newIds: Record<string, boolean> = { ...currentIds }
+        for (const entry of entries) {
+          const { isIntersecting, target } = entry
+          const { id } = target
+          newIds[id] = isIntersecting
+        }
+        return newIds
+      })
+    }
+
+    const observerCallbackSticky: IntersectionObserverCallback = (entries) => {
+      setCurrentIds((currentIds) => {
+        let stickyHeaderSet = false
+        const newIds: Record<string, boolean> = { ...currentIds }
+        for (const entry of entries) {
+          const { isIntersecting, target } = entry
+          const { id, firstElementChild: first } = target
+          newIds[id] = isIntersecting
+          if (!stickyHeaderSet && isIntersecting && first?.textContent && ["H1", "H2"].includes(first.nodeName)) {
+            stickyHeaderSet = true
+            setStickyHeader(first.id === "overview" ? "Overview" : first.textContent)
+          }
+        }
+        if (Object.values(newIds).every((v) => !v)) {
+          setStickyHeader("")
+        }
+        return newIds
+      })
+    }
+
+    const sectionsObserver = new IntersectionObserver(updateSticky ? observerCallbackSticky : observerCallback, {
+      rootMargin: "-15% 0% -85%",
+    })
+
+    headings.forEach((h) => {
+      const section = document.getElementById(h.slug)
+      if (section) {
+        sectionsObserver.observe(section)
+      }
+    })
+    return () => sectionsObserver.disconnect()
+  }, [headings])
+
   return (
     <nav className={styles.container}>
-      <ContentObserver headings={headings} shouldUpdate={$shouldUpdateToc} />
       <ul>
-        {headings.map((h) => (
-          <li key={h.slug}>
-            <a
-              href={`#${h.slug}`}
-              className={`${styles.headerLink}${h.depth && h.depth > 2 ? ` ${styles[`depth-${h.depth}`]}` : ""}
-              ${$currentIds[h.slug] ? ` ${styles.active}` : ""}`}
-            >
-              {h.depth > 1 ? h.text : "Overview"}
-            </a>
-          </li>
-        ))}
+        {headings.map((h) => {
+          const className = clsx(
+            styles.headerLink,
+            h.depth > 2 && styles[`depth-${h.depth}`],
+            currentIds[h.slug] && styles.active
+          )
+          return (
+            <li key={h.slug}>
+              <a href={`#${h.slug}`} className={className}>
+                {h.depth > 1 ? h.text : "Overview"}
+              </a>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   )
