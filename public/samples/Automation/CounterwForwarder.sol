d682d0a42c525cbf3771872ee58020c9154574df
ritika-b <ritika.bharati@smartcontract.com>
2023-11-13 23:10:54 -0500

    add forwarder code

---
+++ public/samples/Automation/CounterwForwarder.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+/**
+ * @dev Example contract which uses the Forwarder
+ *
+ * @notice important to implement {AutomationCompatibleInterface}
+ */
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+import "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CounterwForwarder is AutomationCompatibleInterface, Ownable {
+    uint256 public counter; // counter counts the number of upkeeps performed
+    uint256 public interval; // interval specifies the time between upkeeps
+    uint256 public lastTimeStamp; // lastTimeStamp tracks the last upkeep performed
+    address public s_forwarderAddress;
+
+    constructor(uint256 updateInterval) {
+        interval = updateInterval;
+    }
+
+    function checkUpkeep(
+        bytes calldata /*checkData*/
+    ) external override returns (bool, bytes memory) {
+        bool needsUpkeep = (block.timestamp - lastTimeStamp) > interval;
+        return (needsUpkeep, bytes(""));
+    }
+
+    function performUpkeep(bytes calldata /*performData*/) external override {
+        require(
+            msg.sender == s_forwarderAddress,
+            "This address does not have permission to call performUpkeep"
+        );
+        lastTimeStamp = block.timestamp;
+        counter = counter + 1;
+    }
+
+    /// @notice Set the address that `performUpkeep` is called from
+    /// @dev Only callable by the owner
+    /// @param forwarderAddress the address to set
+    function setForwarderAddress(address forwarderAddress) external onlyOwner {
+        s_forwarderAddress = forwarderAddress;
+    }
+}
