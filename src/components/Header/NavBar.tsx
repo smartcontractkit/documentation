77a4a941a679fccdd2215c3f913998338818ad22
Alejandro Larumbe <alejandro@smartcontract.com>
2023-11-17 12:28:37 +1300

    Weglot initialization script after hydration

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -24,6 +24,42 @@ export const NavBar = ({ path, showSearch = true }: { path: string; showSearch?:
     }
   }
 
+  React.useEffect(() => {
+    if (
+      !window.location.hostname.includes("localhost") &&
+      !window.location.hostname.includes("documentation-private-git-")
+    ) {
+      const script = document.createElement("script")
+      script.src = "https://cdn.weglot.com/weglot.min.js"
+      script.async = true
+      script.onload = () => {
+        Weglot.initialize({
+          api_key: "wg_bc56a95905bfa8990f449554339e82be8",
+          switchers: [
+            {
+              button_style: {
+                full_name: false,
+                with_name: true,
+                is_dropdown: true,
+                with_flags: false,
+                flag_type: "circle",
+              },
+              location: {
+                target: "#weglot",
+                sibling: null,
+              },
+            },
+          ],
+        })
+      }
+      document.body.appendChild(script)
+
+      return () => {
+        document.body.removeChild(script)
+      }
+    }
+  }, [])
+
   return (
     <span ref={navRef}>
       <Nav
