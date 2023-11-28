52cdfa5b679c2609b40a61fe8686b2d6250c067e
Dwight Lyle <dwightjl@gmail.com>
2023-11-28 12:42:20 -0700

    Lint

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -4,6 +4,5 @@ export const NotificationData: BannerContent = {
   description: "Priority Migration for Chainlink Staking v0.2 is now live.",
   type: "info",
   linkText: "Migrate your LINK today.",
-  linkUrl:
-    "https://staking.chain.link/?utm_medium=referral&utm_source=chainlink-docs&utm_campaign=staking-v02",
+  linkUrl: "https://staking.chain.link/?utm_medium=referral&utm_source=chainlink-docs&utm_campaign=staking-v02",
 }
