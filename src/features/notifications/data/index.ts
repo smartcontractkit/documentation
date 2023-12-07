ef1b6304e9e976672b9fc334115ee4c07b9cebd4
ritika-b <ritika.bharati@smartcontract.com>
2023-12-07 18:12:46 -0500

    lint fix

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -4,5 +4,6 @@ export const NotificationData: BannerContent = {
   description: "Chainlink Data Streams have officially launched on mainnet.",
   type: "info",
   linkText: "Sign up for early access.",
-  linkUrl: "https://chainlinkcommunity.typeform.com/datastreams?utm_medium=referral&utm_source=chainlink-docs&ref_id=docs",
+  linkUrl:
+    "https://chainlinkcommunity.typeform.com/datastreams?utm_medium=referral&utm_source=chainlink-docs&ref_id=docs",
 }
