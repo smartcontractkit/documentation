29c069d1b8d1e04ade377c6b5b6d819fb56b8b1c
CLJosh <92806617+CLJosh@users.noreply.github.com>
2023-12-07 12:16:46 -0500

    update banner (#1686)

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -1,8 +1,8 @@
 import { BannerContent } from "../components/HeaderBanner"
 
 export const NotificationData: BannerContent = {
-  description: "Priority Migration for Chainlink Staking v0.2 is now live.",
+  description: "Early Access for Chainlink Staking v0.2 is now live.",
   type: "info",
-  linkText: "Migrate your LINK today.",
+  linkText: "Stake your LINK today.",
   linkUrl: "https://staking.chain.link/?utm_medium=referral&utm_source=chainlink-docs&utm_campaign=staking-v02",
 }
