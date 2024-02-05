f61dd7069f216ebadef4f524d3854515ace1444c
aelmanaa <aelmanaa@gmail.com>
2024-02-05 20:33:26 +0100

    Show abi.encoding/decoding example

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -546,6 +546,10 @@ export const SIDEBAR: Partial<Record<Sections, SectionEntry[]>> = {
           title: "Using Imports with Functions",
           url: "chainlink-functions/tutorials/importing-packages",
         },
+        {
+          title: "Returning multiple responses and decoding them in your smart contract",
+          url: "chainlink-functions/tutorials/abi-decoding",
+        },
       ],
     },
     {
