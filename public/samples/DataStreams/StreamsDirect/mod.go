d85ab370a6ead624dba6c2da17e1e4a774d3cb13
Karim <98668332+khadni@users.noreply.github.com>
2024-02-14 19:32:44 +0100

    DS update

--- public/samples/DataStreams/StreamsDirect/mod.go
+++ public/samples/DataStreams/StreamsDirect/mod.go
@@ -3,22 +3,22 @@ module data-streams-direct
 go 1.21
 
 require (
-	github.com/ethereum/go-ethereum v1.12.2
-	github.com/pkg/errors v0.9.1
-	github.com/smartcontractkit/chainlink/v2 v2.2.1-0.20230823171354-1ead9ee6f6bb
+	github.com/ethereum/go-ethereum v1.12.2 // Ethereum blockchain interaction library
+	github.com/pkg/errors v0.9.1 // Library for handling errors
+	github.com/smartcontractkit/chainlink/v2 v2.2.1-0.20230823171354-1ead9ee6f6bb // Chainlink core components library
 )
 
 replace (
-	//// needed to address mismatch between cosmosSDK and hdevalence/ed25519consensus
+	// Resolves version mismatch between cosmosSDK and hdevalence/ed25519consensus
 	filippo.io/edwards25519 => filippo.io/edwards25519 v1.0.0-rc.1
 
-	// updating CosmWasm to v1.2.4 which brings ARM support
+	// Adds ARM support by updating CosmWasm to v1.2.4
 	github.com/CosmWasm/wasmvm => github.com/CosmWasm/wasmvm v1.2.4
 
 	//// Fix go mod tidy issue for ambiguous imports from go-ethereum
 	//// See https://github.com/ugorji/go/issues/279
 	github.com/btcsuite/btcd => github.com/btcsuite/btcd v0.22.1
 
-	// replicating the replace directive on cosmos SDK
+	// Aligns protobuf version with cosmos SDK requirements
 	github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
 )
