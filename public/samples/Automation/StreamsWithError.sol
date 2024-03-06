026e14b4426e70bbe2916ecb8fcf61420ef76a4a
thedriftofwords <crystal.gomes@gmail.com>
2024-03-06 00:31:57 -0500

    Swap diagram; code edits

--- public/samples/Automation/StreamsWithError.sol
+++ public/samples/Automation/StreamsWithError.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {StreamsLookupCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/interfaces/StreamsLookupCompatibleInterface.sol";
+import {ILogAutomation, Log} from "@chainlink/contracts/src/v0.8/automation/interfaces/ILogAutomation.sol";
+import {IRewardManager} from "@chainlink/contracts/src/v0.8/llo-feeds/interfaces/IRewardManager.sol";
+import {IVerifierFeeManager} from "@chainlink/contracts/src/v0.8/llo-feeds/interfaces/IVerifierFeeManager.sol";
+import {IERC20} from "@chainlink/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/interfaces/IERC20.sol";
+import {Common} from "@chainlink/contracts/src/v0.8/libraries/Common.sol";
+
 /**
  * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
  * DO NOT USE THIS CODE IN PRODUCTION.
@@ -10,84 +17,8 @@ pragma solidity ^0.8.19;
 ////////////////////////INTERFACES
 //////////////////////////////////
 
-interface StreamsLookupCompatibleInterface {
-    error StreamsLookup(
-        string feedParamKey,
-        string[] feeds,
-        string timeParamKey,
-        uint256 time,
-        bytes extraData
-    );
-
-    /**
-     * @notice any contract which wants to utilize FeedLookup feature needs to
-     * implement this interface as well as the automation compatible interface.
-     * @param values an array of bytes returned from Mercury endpoint.
-     * @param extraData context data from feed lookup process.
-     * @return upkeepNeeded boolean to indicate whether the keeper should call performUpkeep or not.
-     * @return performData bytes that the keeper should call performUpkeep with, if
-     * upkeep is needed. If you would like to encode data to decode later, try `abi.encode`.
-     */
-    function checkCallback(
-        bytes[] memory values,
-        bytes memory extraData
-    ) external view returns (bool upkeepNeeded, bytes memory performData);
-}
-
-struct Log {
-    uint256 index;
-    uint256 timestamp;
-    bytes32 txHash;
-    uint256 blockNumber;
-    bytes32 blockHash;
-    address source;
-    bytes32[] topics;
-    bytes data;
-}
-
 interface IERC20 {
     function approve(address spender, uint256 amount) external returns (bool);
-}
-
-interface ILogAutomation {
-    /**
-     * @notice method that is simulated by the keepers to see if any work actually
-     * needs to be performed. This method does does not actually need to be
-     * executable, and since it is only ever simulated it can consume lots of gas.
-     * @dev To ensure that it is never called, you may want to add the
-     * cannotExecute modifier from KeeperBase to your implementation of this
-     * method.
-     * @param log the raw log data matching the filter that this contract has
-     * registered as a trigger
-     * @param checkData user-specified extra data to provide context to this upkeep
-     * @return upkeepNeeded boolean to indicate whether the keeper should call
-     * performUpkeep or not.
-     * @return performData bytes that the keeper should call performUpkeep with, if
-     * upkeep is needed. If you would like to encode data to decode later, try
-     * `abi.encode`.
-     */
-    function checkLog(
-        Log calldata log,
-        bytes memory checkData
-    ) external returns (bool upkeepNeeded, bytes memory performData);
-
-    /**
-     * @notice method that is actually executed by the keepers, via the registry.
-     * The data returned by the checkUpkeep simulation will be passed into
-     * this method to actually be executed.
-     * @dev The input to this method should not be trusted, and the caller of the
-     * method should not even be restricted to any single registry. Anyone should
-     * be able call it, and the input should be validated, there is no guarantee
-     * that the data passed in is the performData returned from checkUpkeep. This
-     * could happen due to malicious keepers, racing keepers, or simply a state
-     * change while the performUpkeep transaction is waiting for confirmation.
-     * Always validate the data passed in.
-     * @param performData is the data which was passed back from the checkData
-     * simulation. If it is encoded, it can easily be decoded into other types by
-     * calling `abi.decode`. This data should not be trusted, and should be
-     * validated against the contract's current state.
-     */
-    function performUpkeep(bytes calldata performData) external;
 }
 
 interface IFeeManager {
@@ -156,7 +87,7 @@ contract StreamsLookupChainlinkAutomation is
         uint192 nativeFee; // Base cost to validate a transaction using the report, denominated in the chain’s native token (WETH/ETH)
         uint192 linkFee; // Base cost to validate a transaction using the report, denominated in LINK
         uint32 expiresAt; // Latest timestamp where the report can be verified on-chain
-        int192 price; // DON consensus median price, carried to 8 decimal places
+        int192 price; // DON consensus median price, carried to 18 decimal places
     }
 
     struct PremiumReport {
@@ -166,7 +97,7 @@ contract StreamsLookupChainlinkAutomation is
         uint192 nativeFee; // Base cost to validate a transaction using the report, denominated in the chain’s native token (WETH/ETH)
         uint192 linkFee; // Base cost to validate a transaction using the report, denominated in LINK
         uint32 expiresAt; // Latest timestamp where the report can be verified on-chain
-        int192 price; // DON consensus median price, carried to 8 decimal places
+        int192 price; // DON consensus median price, carried to 18 decimal places
         int192 bid; // Simulated price impact of a buy order up to the X% depth of liquidity utilisation
         int192 ask; // Simulated price impact of a sell order up to the X% depth of liquidity utilisation
     }
@@ -183,7 +114,7 @@ contract StreamsLookupChainlinkAutomation is
     string public constant STRING_DATASTREAMS_FEEDLABEL = "feedIDs";
     string public constant STRING_DATASTREAMS_QUERYLABEL = "timestamp";
     string[] public feedIds = [
-        "0x00027bbaff688c906a3e20a34fe951715d1018d262a5b66e38eda027a674cd1b View in Tenderly " // Ex. Basic ETH/USD price report
+        "0x00027bbaff688c906a3e20a34fe951715d1018d262a5b66e38eda027a674cd1b" // Ex. Basic ETH/USD price report
     ];
 
     constructor(address _verifier) {
