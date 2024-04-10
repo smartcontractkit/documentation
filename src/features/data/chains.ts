f2dda471b32c0d1bbbdaa1f0a270cececc223b18
Karim <98668332+khadni@users.noreply.github.com>
2024-04-10 11:13:40 +0200

    DS Update

--- src/features/data/chains.ts
+++ src/features/data/chains.ts
@@ -193,7 +193,8 @@ export const CHAINS: Chain[] = [
         name: "Arbitrum Mainnet",
         explorerUrl: "https://arbiscan.io/address/%s",
         networkType: "mainnet",
-        rddUrl: "https://reference-data-directory.vercel.app/feeds-ethereum-mainnet-arbitrum-1.json",
+        rddUrl:
+          "https://reference-data-directory-2jy03bftt-chainlinklabs.vercel.app/feeds-ethereum-mainnet-arbitrum-1.json",
         queryString: "arbitrum-mainnet",
         tags: ["nftFloorPrice", "streams"],
       },
@@ -201,7 +202,8 @@ export const CHAINS: Chain[] = [
         name: "Arbitrum Sepolia",
         explorerUrl: "https://sepolia.arbiscan.io/address/%s",
         networkType: "testnet",
-        rddUrl: "https://reference-data-directory.vercel.app/feeds-ethereum-testnet-sepolia-arbitrum-1.json",
+        rddUrl:
+          "https://reference-data-directory-2jy03bftt-chainlinklabs.vercel.app/feeds-ethereum-testnet-sepolia-arbitrum-1.json",
         queryString: "arbitrum-sepolia",
         tags: ["rates", "streams"],
       },
