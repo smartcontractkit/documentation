50b379a549c74070bf616f80928f71896aba3b52
aelmanaa <aelmanaa@gmail.com>
2024-04-11 18:39:30 +0200

    nit

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -4,6 +4,5 @@ export const NotificationData: BannerContent = {
   description: "Introducing Transporter, a new bridging app powered by CCIP.",
   type: "info",
   linkText: "Go cross-chain.",
-  linkUrl:
-    "https://transporter.io?utm_campaign=ga&utm_medium=referral&utm_source=chainlink-docs&utm_content=banner",
+  linkUrl: "https://transporter.io?utm_campaign=ga&utm_medium=referral&utm_source=chainlink-docs&utm_content=banner",
 }
