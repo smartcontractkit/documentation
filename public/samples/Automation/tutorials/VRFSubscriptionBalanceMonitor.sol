de2a5738a7c77dbc077850a315da83064bd5f314
thedriftofwords <crystal.gomes@gmail.com>
2024-04-25 12:19:10 -0400

    try 0.8.19

--- public/samples/Automation/tutorials/VRFSubscriptionBalanceMonitor.sol
+++ public/samples/Automation/tutorials/VRFSubscriptionBalanceMonitor.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.20;
+pragma solidity 0.8.19;
 
 import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import "@chainlink/contracts/src/v0.8/automation/interfaces/KeeperCompatibleInterface.sol";
