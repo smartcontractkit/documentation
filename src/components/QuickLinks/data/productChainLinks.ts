d0d8f6efb523961a042e69ce37c6d76a7127ca56
aelmanaa <aelmanaa@gmail.com>
2024-08-01 20:51:32 +0100

    config

--- src/components/QuickLinks/data/productChainLinks.ts
+++ src/components/QuickLinks/data/productChainLinks.ts
@@ -33,6 +33,7 @@ export const productChainLinks: ProductChainLinks = {
       polygon: "/ccip/supported-networks/v1_2_0/mainnet#polygon-mainnet",
       wemix: "/ccip/supported-networks/v1_2_0/mainnet#wemix-mainnet",
       mode: "/ccip/supported-networks/v1_2_0/mainnet#mode-mainnet",
+      metis: "/ccip/supported-networks/v1_2_0/mainnet#metis-mainnet",
     },
   },
   "Data Feeds": {
