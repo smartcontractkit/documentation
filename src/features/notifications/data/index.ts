4159e867a06df8afb6c8484d89ea1ad9e8f97437
CLJosh <joshua.azar@smartcontract.com>
2024-08-21 12:55:09 -0400

    testing commit

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -3,6 +3,6 @@ import { BannerContent } from "../components/HeaderBanner"
 export const NotificationData: BannerContent = {
   description: "Developer Passes are now available for Chainlink SmartCon.",
   type: "info",
-  linkText: "Get yours today.",
+  linkText: "Get your ticket.",
   linkUrl: "https://smartcon.chain.link/home?utm_medium=referral&utm_source=chainlink-docs&utm_campaign=smartcon-2024",
 }
