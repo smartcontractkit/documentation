10f4e8f5f2931018572615a38037ce414a057a5b
Karim H. <98668332+khadni@users.noreply.github.com>
2024-08-26 22:38:04 +0200

    Add CCIP metrics (#2029)
    * Router address, chain selectors, onRamp address metrics
    
    * Add transferable tokens
    
    * Add Minted Tokens
    
    * Merge main and resolve conflicts
    
    * Refactor Address & CopyText components
    
    * Use canonical values in events
    
    * Refactor/standardize event & params structure
    
    * Add feeToken interaction

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -7,10 +7,32 @@ export type Props = {
   endLength?: number
   urlClass?: string
   urlId?: string
+  eventName?: string
+  additionalInfo?: Record<string, string>
 }
 
-const AddressComponent = ({ contractUrl, address, endLength, urlClass, urlId }: Props) => {
+const AddressComponent = ({
+  contractUrl,
+  address,
+  endLength,
+  urlClass,
+  urlId,
+  eventName,
+  additionalInfo = {}, // Default to an empty object if not provided
+}: Props) => {
   address = address || contractUrl.split("/").pop()
+
+  const handleClick = (e) => {
+    e.preventDefault()
+
+    if (eventName !== undefined) {
+      const dataLayerEvent = {
+        event: eventName,
+        ...additionalInfo,
+      }
+      window.dataLayer.push(dataLayerEvent)
+    }
+  }
 
   return (
     <span className={`addressContainer ${urlClass || ""}`} id={urlId}>
@@ -21,6 +43,7 @@ const AddressComponent = ({ contractUrl, address, endLength, urlClass, urlId }: 
         className={clsx("copyBtn", "copy-iconbutton")}
         style={{ height: "16px", width: "16px", minWidth: "12px" }}
         data-clipboard-text={address}
+        onClick={handleClick}
       >
         <img src="/assets/icons/copyIcon.svg" alt="Copy to clipboard" />
       </button>
@@ -38,6 +61,7 @@ const AddressComponent = ({ contractUrl, address, endLength, urlClass, urlId }: 
           align-items: center;
           gap: var(--space-1x);
           word-break: break-word;
+          margin-top: 0;
         }
 
         .copyBtn {
