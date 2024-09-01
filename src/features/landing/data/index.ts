8ee50fa1c6625874c75685eec3cee33150480a17
aelmanaa <aelmanaa@gmail.com>
2024-09-01 15:55:42 +0200

    config

--- src/features/landing/data/index.ts
+++ src/features/landing/data/index.ts
@@ -42,6 +42,7 @@ export const evmProducts = [
       { id: "gnosis-chain", title: "Gnosis Chain" },
       { id: "mode", title: "Mode" },
       { id: "metis", title: "Metis" },
+      { id: "zksync", title: "zkSync" },
     ],
   },
   {
