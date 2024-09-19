97aad1598016664f981271b0a5366e90df49d96e
Simone Cuomo <simone@thisdot.co>
2024-09-19 08:24:43 +0100

    Adjust navigation in pages that do not use docLayout

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -12,14 +12,26 @@ export const NavBar = ({ path, showSearch = true }: { path: string; showSearch?:
 
   const onHideChange = (hidden: boolean) => {
     if (navRef.current) {
+      /* This method calculate the height required for the sticky headers within the page content.
+      / - The height is determined by two different factors:
+      / - if the page has been scrolled down and the header is hidden
+      / - if the page is a inner doc page or part of the "pathWithoutDocNav" or not
+      */
+      const pathWithoutDocNav = ["/quickstarts/"]
+      const shouldAddDocNavigation = !pathWithoutDocNav.some((p) => path.includes(p))
       const innerDocNavHeight = 56
-      const height = (navRef.current as HTMLElement).clientHeight + innerDocNavHeight
+      let height = (navRef.current as HTMLElement).clientHeight
+      let baseHeightNoNav = 0
+      if (shouldAddDocNavigation) {
+        height += innerDocNavHeight
+        baseHeightNoNav += innerDocNavHeight
+      }
       const elements = document.body.querySelectorAll("[data-sticky]")
       elements.forEach((e: HTMLElement) => {
         if (!e.classList.contains(styles.animateTop)) {
           e.classList.add(styles.animateTop)
         }
-        e.style.top = `${hidden ? innerDocNavHeight : height}px`
+        e.style.top = `${hidden ? baseHeightNoNav : height}px`
       })
       setNavBarInfo({ hidden, height })
     }
