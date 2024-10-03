808e47ef2b3ef600d8c15abe0e2b532319a0065f
aelmanaa <aelmanaa@gmail.com>
2024-10-03 09:22:16 +0200

    update

--- src/config/data/ccip/types.ts
+++ src/config/data/ccip/types.ts
@@ -52,6 +52,18 @@ export type ChainConfig = {
     address: string
     version: string
   }
+  armProxy: {
+    address: string
+    version: string
+  }
+  registryModule?: {
+    address: string
+    version: string
+  }
+  tokenAdminRegistry?: {
+    address: string
+    version: string
+  }
 }
 
 export type ChainsConfig = {
