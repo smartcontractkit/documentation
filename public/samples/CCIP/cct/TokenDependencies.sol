291fd1f5b96a147c84cfe43565fdda22616d5ee0
aelmanaa <aelmanaa@gmail.com>
2024-12-04 16:15:10 +0100

    update

---
+++ public/samples/CCIP/cct/TokenDependencies.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+// solhint-disable no-unused-import
+import {BurnMintERC677} from "@chainlink/contracts-ccip/src/v0.8/shared/token/ERC677/BurnMintERC677.sol";
