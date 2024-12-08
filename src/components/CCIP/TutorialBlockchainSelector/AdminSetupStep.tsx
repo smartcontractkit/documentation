fc36e055ab01e65ad05a0fdd7112492873498df4
aelmanaa <aelmanaa@gmail.com>
2024-12-08 16:47:37 +0100

    update

--- src/components/CCIP/TutorialBlockchainSelector/AdminSetupStep.tsx
+++ src/components/CCIP/TutorialBlockchainSelector/AdminSetupStep.tsx
@@ -26,39 +26,51 @@ export const AdminSetupStep = ({ chain }: AdminSetupStepProps) => {
         <TutorialStep title="Register as Admin" checkbox={<StepCheckbox stepId={stepId} subStepId="admin-claimed" />}>
           <ol className={styles.instructions}>
             <li>
+              In the "Deploy & Run Transactions" tab, select <strong>RegistryModuleOwnerCustom</strong> contract
+            </li>
+            <li>
+              Use "At Address" with:
               <div className={styles.contractInfo}>
                 <strong>Contract:</strong> RegistryModuleOwnerCustom
                 <NetworkAddress type="registryModule" chain={chain} />
               </div>
             </li>
+            <li>The RegistryModuleOwnerCustom will be displayed in the "Deployed Contracts" section</li>
+            <li>Click on the RegistryModuleOwnerCustom contract address to open the contract details</li>
             <li>
-              <div className={styles.actionDetails}>
-                <div className={styles.actionTitle}>
-                  Call <code>registerAdminViaOwner</code> with:
-                </div>
-                <div className={styles.parameter}>
-                  <span className={styles.paramName}>token</span>
-                  <StoredContractAddress type="token" chain={chain} />
-                </div>
+              Call <code>registerAdminViaOwner</code> with:
+              <div className={styles.parameter}>
+                <span className={styles.paramName}>token</span>
+                <StoredContractAddress type="token" chain={chain} />
               </div>
             </li>
+            <li>Confirm the transaction in MetaMask</li>
           </ol>
         </TutorialStep>
 
         <TutorialStep title="Accept Admin Role" checkbox={<StepCheckbox stepId={stepId} subStepId="admin-accepted" />}>
-          <div className={styles.contractInfo}>
-            <strong>Contract:</strong> TokenAdminRegistry
-            <NetworkAddress type="tokenAdminRegistry" chain={chain} />
-          </div>
-          <div className={styles.actionDetails}>
-            <div className={styles.actionTitle}>
+          <ol className={styles.instructions}>
+            <li>
+              In the "Deploy & Run Transactions" tab, select <strong>TokenAdminRegistry</strong> contract
+            </li>
+            <li>
+              Use "At Address" with:
+              <div className={styles.contractInfo}>
+                <strong>Contract:</strong> TokenAdminRegistry
+                <NetworkAddress type="tokenAdminRegistry" chain={chain} />
+              </div>
+            </li>
+            <li>The TokenAdminRegistry will be displayed in the "Deployed Contracts" section</li>
+            <li>Click on the TokenAdminRegistry contract address to open the contract details</li>
+            <li>
               Call <code>acceptAdminRole</code> with:
-            </div>
-            <div className={styles.parameter}>
-              <span className={styles.paramName}>token</span>
-              <StoredContractAddress type="token" chain={chain} />
-            </div>
-          </div>
+              <div className={styles.parameter}>
+                <span className={styles.paramName}>token</span>
+                <StoredContractAddress type="token" chain={chain} />
+              </div>
+            </li>
+            <li>Confirm the transaction in MetaMask</li>
+          </ol>
         </TutorialStep>
       </ol>
     </>
