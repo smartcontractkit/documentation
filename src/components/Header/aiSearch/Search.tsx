6f1b7402c5d5fdcf234678f902891b7c046cb824
Simone Cuomo <simone@thisdot.co>
2025-01-30 10:02:02 +0000

    Add Specific links and upgrade to V 0.8

--- src/components/Header/aiSearch/Search.tsx
+++ src/components/Header/aiSearch/Search.tsx
@@ -6,5 +6,18 @@ export const Search = ({
 }: {
   algoliaVars: { algoliaAppId: string; algoliaPublicApiKey: string }
 }) => {
-  return <SearchButton algoliaAppId={algoliaAppId} algoliaPublicApiKey={algoliaPublicApiKey} />
+  return (
+    <SearchButton
+      algoliaAppId={algoliaAppId}
+      algoliaPublicApiKey={algoliaPublicApiKey}
+      popularCards={[
+        {
+          url: "https://dev.chain.link/resources/quickstarts",
+          imgSrc: "/images/algolia/quick-start.png",
+          label: "Quickstarts",
+        },
+        { url: "https://dev.chain.link/tools", imgSrc: "/images/algolia/tools.png", label: "Tools" },
+      ]}
+    />
+  )
 }
