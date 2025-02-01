20009b714502428e5ab29fc173ebed02668a22aa
gfletcher-cll <grace.fletcher@smartcontract.com>
2025-02-01 12:13:04 -0800

    Update web3Providers.ts
    add treasure mainnet and testnet topaz

--- src/config/web3Providers.ts
+++ src/config/web3Providers.ts
@@ -80,6 +80,8 @@ export const chainToProvider: Record<SupportedChain, () => providers.Provider> =
   SEI_TESTNET: () => new providers.JsonRpcProvider("https://evm-rpc-testnet.sei-apis.com"),
   CORE_TESTNET: () => new providers.JsonRpcProvider("https://rpc.test2.btcs.network/"),
   MONAD_TESTNET: () => new providers.JsonRpcProvider("https://monad.xyz"),
+  TREASURE_MAINNET: () => new providers.JsonRpcProvider("https://rpc.treasure.lol"),
+  TREASURE_TESTNET: () => new providers.JsonRpcProvider("https://rpc.topaz.treasure.lol"),
 }
 
 export const getRpcUrlForChain = (chain: SupportedChain): string => {
