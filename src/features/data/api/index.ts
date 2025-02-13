28feff26d023bcb748cf31114165f33ac93aa301
Karim <98668332+khadni@users.noreply.github.com>
2025-02-13 09:37:16 -0500

    update

--- src/features/data/api/index.ts
+++ src/features/data/api/index.ts
@@ -42,12 +42,7 @@ export const getFeedsMetadata = (url: string): Promise<ChainMetadata[]> => {
   return fetch(url).then((res) => res.json())
 }
 
-type ChainNetwork = Chain["networks"][0]
-type NetworkWithMetadata = ChainNetwork & { metadata?: ChainMetadata[] }
-
-export const getChainMetadata = async (
-  chain: Chain
-): Promise<Omit<Chain, "networks"> & { networks: (NetworkWithMetadata | undefined)[] }> => {
+export const getChainMetadata = async (chain: Chain): Promise<ChainMetadata | any> => {
   const requests = chain.networks.map((network) =>
     network?.rddUrl
       ? getFeedsMetadata(network?.rddUrl).then((metadata) => ({
