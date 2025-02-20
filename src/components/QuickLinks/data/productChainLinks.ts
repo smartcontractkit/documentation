ae380d6d015b3fee129e04b2fa9cffb1e75254a2
Karim H. <98668332+khadni@users.noreply.github.com>
2025-02-20 19:13:38 +0100

    DF integrations - Ink, Mantle, Ronin, Sonic testnets (#2373)
    * df integrations feb 17
    
    * Merge remote-tracking branch 'origin' into df-integrations-feb-17
    
    * changelog entry
    
    * ink mainnet

--- src/components/QuickLinks/data/productChainLinks.ts
+++ src/components/QuickLinks/data/productChainLinks.ts
@@ -80,6 +80,7 @@ export const productChainLinks: ProductChainLinks = {
       fantom: "/data-feeds/price-feeds/addresses?network=fantom",
       "gnosis-chain": "/data-feeds/price-feeds/addresses?network=gnosis-chain",
       hedera: "/data-feeds/price-feeds/addresses?network=hedera",
+      ink: "/data-feeds/price-feeds/addresses?network=ink",
       linea: "/data-feeds/price-feeds/addresses?network=linea",
       mantle: "/data-feeds/price-feeds/addresses?network=mantle",
       metis: "/data-feeds/price-feeds/addresses?network=metis",
