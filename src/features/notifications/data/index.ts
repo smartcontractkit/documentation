ef4f13dd9c1314e111ff3bee906ace55beeb2dc6
gfletcher-cll <grace.fletcher@smartcontract.com>
2025-05-13 10:05:36 -0700

    Update index.ts
    update announcement banner content

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -1,8 +1,8 @@
 import { BannerContent } from "../components/HeaderBanner.tsx"
 
 export const NotificationData: BannerContent = {
-  description: "Finance is on at SmartCon in NYC.",
+  description: "Chainlink Rewards: Genesis Starts With SXT.",
   type: "info",
-  linkText: "Get notified when tickets go live.",
-  linkUrl: "https://smartcon.chain.link?utm_source=docs",
+  linkText: "Learn how to build with SXT.",
+  linkUrl: "https://go.chain.link/masterclasses/build-with-space-and-time?utm_medium=referral&utm_source=chainlink-docs&utm_campaign=sxt-linklab",
 }
