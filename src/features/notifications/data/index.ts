5ada0dc86737db249ea63cfb488bb0da698bf720
Karim <98668332+khadni@users.noreply.github.com>
2025-05-13 13:40:27 -0500

    nit formatting

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -4,5 +4,6 @@ export const NotificationData: BannerContent = {
   description: "Chainlink Rewards: Genesis Starts With SXT.",
   type: "info",
   linkText: "Learn how to build with SXT.",
-  linkUrl: "https://go.chain.link/masterclasses/build-with-space-and-time?utm_medium=referral&utm_source=chainlink-docs&utm_campaign=sxt-linklab",
+  linkUrl:
+    "https://go.chain.link/masterclasses/build-with-space-and-time?utm_medium=referral&utm_source=chainlink-docs&utm_campaign=sxt-linklab",
 }
