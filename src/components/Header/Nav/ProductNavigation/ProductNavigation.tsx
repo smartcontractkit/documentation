4ad4d1fcc56cceba43bb8d3c8309a65d325734c6
Simone Cuomo <simone@thisdot.co>
2025-05-16 14:02:23 +0100

    Add code required for AI to load

--- src/components/Header/Nav/ProductNavigation/ProductNavigation.tsx
+++ src/components/Header/Nav/ProductNavigation/ProductNavigation.tsx
@@ -1,11 +1,9 @@
 import { SubProductsNav, ProductsNav } from "../config.tsx"
-import { SearchTrigger } from "../NavBar.tsx"
 import { ProductNavigation as Desktop } from "./Desktop/ProductNavigation.tsx"
 import { ProductNavigation as Mobile } from "./Mobile/ProductNavigation.tsx"
 
 type Props = {
   path: string
-  searchTrigger?: SearchTrigger
   setNavMenuOpen: (navMenuOpen: boolean) => void
   productsNav: ProductsNav
   subProductsNav?: SubProductsNav
