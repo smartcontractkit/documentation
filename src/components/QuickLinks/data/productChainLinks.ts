8e69d9748230cd7043c7ca8d034ec2408d742668
aelmanaa <aelmanaa@gmail.com>
2025-09-17 18:41:03 +0200

    update chains

--- src/components/QuickLinks/data/productChainLinks.ts
+++ src/components/QuickLinks/data/productChainLinks.ts
@@ -88,6 +88,8 @@ export const productChainLinks: ProductChainLinks = {
       "0g": "/ccip/directory/testnet/chain/0g-testnet-galileo",
       hyperevm: "/ccip/directory/mainnet/chain/hyperevm-mainnet",
       etherlink: "/ccip/directory/testnet/chain/etherlink-testnet",
+      kaia: "/ccip/directory/mainnet/chain/kaia-mainnet",
+      tac: "/ccip/directory/mainnet/chain/tac-mainnet",
     },
   },
   "Data Feeds": {
@@ -292,6 +294,7 @@ export const productChainLinks: ProductChainLinks = {
     "0g": "/resources/link-token-contracts#0g",
     hyperevm: "/resources/link-token-contracts#hyperevm",
     kaia: "/resources/link-token-contracts#kaia",
+    tac: "/resources/link-token-contracts#tac",
   },
 }
 
