2ceb1489fad9ef6782f76a630b94dcf47ea40fcd
Grace Fletcher <grace.fletcher@smartcontract.com>
2025-09-17 09:23:45 -0700

    add three new search capabilities

--- src/components/Header/aiSearch/Search.tsx
+++ src/components/Header/aiSearch/Search.tsx
@@ -40,6 +40,9 @@ function AlgoliaSearch({ algoliaVars }) {
       categoryOrder={["Documentation"]}
       popularCards={popularCards}
       ariaLabel="Open AI search"
+      spotlight={["Documentation"]}
+      categoriesToShow={["Documentation", "CCIP Network"]}
+      hideSuggestions={true}
     />
   )
 }
