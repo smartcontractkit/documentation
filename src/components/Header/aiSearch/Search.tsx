d957434210df7029b13768d880e495dcbb39bb73
gfletcher-cll <grace.fletcher@smartcontract.com>
2025-09-30 13:21:29 -0700

    Update Search.tsx

--- src/components/Header/aiSearch/Search.tsx
+++ src/components/Header/aiSearch/Search.tsx
@@ -41,7 +41,6 @@ function AlgoliaSearch({ algoliaVars }) {
       popularCards={popularCards}
       ariaLabel="Open AI search"
       spotlight={["Documentation"]}
-      baseApiUrl="https://cl-ai-search-api-preview.vercel.app/api"
     />
   )
 }
