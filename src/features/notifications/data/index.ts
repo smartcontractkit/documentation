a3cbd8dcd49f537e7171fbc645bf85277f20b712
Karim H. <98668332+khadni@users.noreply.github.com>
2025-11-04 11:40:48 -0500

    Banner update (#3130)
    * banner update
    
    * banner update

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -1,9 +1,9 @@
 import { BannerContent } from "../components/HeaderBanner.tsx"
 
 export const NotificationData: BannerContent = {
-  description: "Introducing DataLink: Bringing Institutional Market Data Onchain With Launch Partner Deutsche Börse.",
+  description: "The Chainlink Runtime Environment (CRE) Is Now Live.",
   type: "info",
   linkText: "Learn more.",
   linkUrl:
-    "https://blog.chain.link/introducing-datalink/?utm_campaign=datalink&utm_source=chainlink-docs&utm_medium=referral",
+    "https://blog.chain.link/chainlink-runtime-environment-now-live/?utm_source=chainlink-docs&utm_campaign=smartcon-2025&utm_medium=referral",
 }
