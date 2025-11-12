a5884663458e83c972255f42e2e18513bbf25e82
Karim H. <98668332+khadni@users.noreply.github.com>
2025-11-12 11:17:12 -0500

    update banner (#3164)

--- src/features/notifications/data/index.ts
+++ src/features/notifications/data/index.ts
@@ -1,9 +1,9 @@
 import { BannerContent } from "../components/HeaderBanner.tsx"
 
 export const NotificationData: BannerContent = {
-  description: "The Chainlink Runtime Environment (CRE) Is Now Live.",
+  description: "The Chainlink Runtime Environment (CRE) is now live!",
   type: "info",
-  linkText: "Learn more.",
+  linkText: "Start Building.",
   linkUrl:
-    "https://blog.chain.link/chainlink-runtime-environment-now-live/?utm_source=chainlink-docs&utm_campaign=smartcon-2025&utm_medium=referral",
+    "https://chain.link/chainlink-runtime-environment/?utm_source=chainlink-docs&utm_campaign=smartcon-2025&utm_medium=referral",
 }
