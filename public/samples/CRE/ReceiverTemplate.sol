94e06649877256934e49efab0a35cad7bc8ded79
devin distefano <devin.distefano@smartcontract.com>
2026-01-15 13:00:54 -0800

    revert whitespace changes to CRE samples

--- public/samples/CRE/ReceiverTemplate.sol
+++ public/samples/CRE/ReceiverTemplate.sol
@@ -75,7 +75,10 @@ abstract contract ReceiverTemplate is IReceiver, Ownable {
 
   /// @inheritdoc IReceiver
   /// @dev Performs optional validation checks based on which permission fields are set
-  function onReport(bytes calldata metadata, bytes calldata report) external override {
+  function onReport(
+    bytes calldata metadata,
+    bytes calldata report
+  ) external override {
     // Security Check 1: Verify caller is the trusted Chainlink Forwarder (if configured)
     if (s_forwarderAddress != address(0) && msg.sender != s_forwarderAddress) {
       revert InvalidSender(msg.sender, s_forwarderAddress);
