6cada9b6039ee79b5987df8ba3b806f3f3c5c8b8
devin distefano <devin.distefano@smartcontract.com>
2026-03-27 16:09:01 -0500

    self managed feeds page

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -666,6 +666,7 @@ export const SIDEBAR: Partial<Record<Sections, SectionEntry[]>> = {
             },
             { title: "Rate and Volatility Feeds", url: "data-feeds/rates-feeds" },
             { title: "L2 Sequencer Uptime Feeds", url: "data-feeds/l2-sequencer-feeds" },
+            { title: "Self-Managed Feeds", url: "data-feeds/self-managed-feeds" },
           ],
         },
         {
