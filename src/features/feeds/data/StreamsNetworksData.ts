acd089b95d9949ea8a6e897a4311414f19c8bf1c
devin distefano <devin.distefano@smartcontract.com>
2026-09-15 18:46:35 -0500

    arc mainnet data streams proxy added

--- src/features/feeds/data/StreamsNetworksData.ts
+++ src/features/feeds/data/StreamsNetworksData.ts
@@ -90,6 +90,11 @@ export const StreamsNetworksData: NetworkData[] = [
   {
     network: "Arc",
     logoUrl: "/assets/chains/arc.svg",
+    mainnet: {
+      label: "Arc Mainnet",
+      verifierProxy: "0xcE73c8ad08CBDEaCa6078BF0627C8fe0a9a536E7",
+      explorerUrl: "https://explorer.arc.io/address/%s",
+    },
     testnet: {
       label: "Arc Testnet",
       verifierProxy: "0x72790f9eB82db492a7DDb6d2af22A270Dcc3Db64",
